82259e525f271e3886c7e9bbdda43387e490984a
Rong Ling <wumnlr@gmail.com>
2022-11-21 06:20:58 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -1,35 +1,35 @@
 window.TradingData = {
     'Settings': {
-        'currentDayStr': '2022-11-18 6:30', //'2022-11-18 6:30'
+        'currentDayStr': '', //'2022-11-18 6:30'
         'activeProfileName': "momentumEquities", //choppyMarket"; //"momentumEquities";
     },
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
-        '11/18/2022': {
+        '11/21/2022': {
             'stocks': [
-                //'ROST',
-                //'FL',
-                //'COIN',
+                'TSLA', 'DIS',
+                'BABA',
                 'SPY',
+                //'QQQ',
             ],
             'StockCandidates': {
-                'FL': {
-                    highQualityNews: 'earnings', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                'DIS': {
+                    highQualityNews: 'CEO returns', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTrading: false,
                     boxup: 39.5, boxdown: 36.75,
                     longTargets: [],
                     shortTargets: [{ price: 35.1, percentage: 0.1 }]
                 },
-                'COIN': {
-                    highQualityNews: 'BoA downgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                'TSLA': {
+                    highQualityNews: 'recall', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTrading: false,
                     boxup: 49.5, boxdown: 48,
                     longTargets: [],
                     shortTargets: [{ price: 46.2, percentage: 0.2 }]
                 },
-                'ROST': {
-                    highQualityNews: 'earnings and upgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                'BABA': {
+                    highQualityNews: 'china covid increase', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTrading: false,
                     boxup: 114.5, boxdown: 112.5,
                     longTargets: [{ price: 116.97, percentage: 0.1 }],
