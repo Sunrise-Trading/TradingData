101cf6f834e288f43e459929bfce261a2491cfcf
Rong Ling <wumnlr@gmail.com>
2022-11-24 11:50:52 -0800

    update

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -16,41 +16,41 @@ window.TradingData = {
             'StockCandidates': {
                 'JWN': {
                     highQualityNews: 'earnings, lower guidance', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTrading: false,
+                    deferTradingInSeconds: 0,
                     boxup: 21.35, boxdown: 20.6,
                     longTargets: [],
                     shortTargets: [{ price: 20.45, percentage: 0.1 }]
                 },
                 'ADSK': {
                     highQualityNews: 'earnings, price cut', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTrading: false,
+                    deferTradingInSeconds: 0,
                     boxup: 193, boxdown: 189,
                     longTargets: [],
                     shortTargets: [{ price: 188, percentage: 0.1 }]
                 },
                 'AAPL': {
                     highQualityNews: 'ihone shortage', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTrading: false,
+                    deferTradingInSeconds: 0,
                     boxup: 75.5, boxdown: 73,
                     longTargets: [{ price: 116.97, percentage: 0.1 }],
                     shortTargets: [{ price: 77.5, percentage: 0.1 }]
                 },
                 'TSLA': {
                     highQualityNews: 'cybertruck preorder, citi group upgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTrading: false,
+                    deferTradingInSeconds: 0,
                     boxup: 174.5, boxdown: 172,
                     longTargets: [{ price: 174.8, percentage: 0.1 }],
                     shortTargets: [{ price: 171.9, percentage: 0.1 }]
                 },
                 'QQQ': {
                     highQualityNews: 'retails data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTrading: false,
+                    deferTradingInSeconds: 0,
                     longTargets: [{ price: 384.8, percentage: 0.2 }],
                     shortTargets: []
                 },
                 'SPY': {
                     highQualityNews: 'consumer sentiment', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTrading: false,
+                    deferTradingInSeconds: 0,
                     longTargets: [{ price: 384.8, percentage: 0.2 }],
                     shortTargets: []
                 },
