d3335d3765e61f3d5471bfa83a47dae6327a81c0
Rong Ling <wumnlr@gmail.com>
2022-11-30 06:14:48 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -1,14 +1,14 @@
 window.TradingData = {
     'Settings': {
         'currentDayStr': '', //'2022-11-18 6:30'
-        'activeProfileName': "futures", //choppyMarket"; //"momentumEquities";
+        'activeProfileName': "momentumEquities", //choppyMarket"; //"momentumEquities";
     },
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
         '11/30/2022': {
             'stocks': [
-                //'ROKU',
+                'CRWD',
                 'PDD',
                 'BABA',
                 //'SPY',
@@ -16,7 +16,7 @@ window.TradingData = {
             ],
             'StockCandidates': {
                 'PDD': {
-                    highQualityNews: 'upgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    highQualityNews: 'china news, raise price targets', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTradingInSeconds: 0,
                     initialBias: 0,
                     boxup: 78.6, boxdown: 77.5,
@@ -31,13 +31,13 @@ window.TradingData = {
                     longTargets: [],
                     shortTargets: [{ price: 146.7, percentage: 0.1 }]
                 },
-                'ROKU': {
-                    highQualityNews: 'downgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                'CRWD': {
+                    highQualityNews: 'earnings miss', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTradingInSeconds: 80,
                     initialBias: 0,
-                    boxup: 52.2, boxdown: 51.25,
+                    boxup: 116, boxdown: 112,
                     longTargets: [],
-                    shortTargets: [{ price: 50.25, percentage: 0.1 }]
+                    shortTargets: [{ price: 110, percentage: 0.1 }]
                 },
                 'BABA': {
                     highQualityNews: 'china ease lockdown', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
@@ -56,14 +56,14 @@ window.TradingData = {
                     shortTargets: []
                 },
                 'QQQ': {
-                    highQualityNews: 'retails data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTradingInSeconds: 0,
                     initialBias: 0,
                     longTargets: [{ price: 384.8, percentage: 0.2 }],
                     shortTargets: []
                 },
                 'SPY': {
-                    highQualityNews: 'consumer sentiment', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTradingInSeconds: 0,
                     initialBias: 0,
                     longTargets: [{ price: 384.8, percentage: 0.2 }],
