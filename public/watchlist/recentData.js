169ab9f00215ab1addc8419764d65c5333f52e3b
Rong Ling <wumnlr@gmail.com>
2022-12-01 06:27:09 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -1,17 +1,17 @@
 window.TradingData = {
     'Settings': {
         'currentDayStr': '', //'2022-11-18 6:30'
-        'activeProfileName': "futures", //choppyMarket"; //"momentumEquities";
+        'activeProfileName': "momentumEquities", //choppyMarket"; //"momentumEquities";
     },
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
         '12/1/2022': {
             'stocks': [
-                'CRWD',
-                'PDD',
+                'SNOW',
+                'CRM',
                 'BABA',
-                //'AMZN',
+                'TSLA',
                 //'QQQ',
             ],
             'StockCandidates': {
@@ -23,36 +23,36 @@ window.TradingData = {
                     longTargets: [],
                     shortTargets: [{ price: 79.95, percentage: 0.1 }]
                 },
-                'AAPL': {
-                    highQualityNews: 'more produciton issue in Foxconn', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                'SNOW': {
+                    highQualityNews: 'earnings', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTradingInSeconds: 0,
                     initialBias: 0,
-                    boxup: 145.75, boxdown: 144.5,
-                    longTargets: [],
-                    shortTargets: [{ price: 146.7, percentage: 0.1 }]
+                    boxup: 146, boxdown: 140,
+                    longTargets: [{ price: 147.2, percentage: 0.1 }],
+                    shortTargets: []
                 },
-                'CRWD': {
+                'CRM': {
                     highQualityNews: 'earnings miss', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTradingInSeconds: 80,
+                    deferTradingInSeconds: 0,
                     initialBias: 0,
-                    boxup: 116, boxdown: 112,
-                    longTargets: [{ price: 119.85, percentage: 0.1 }],
-                    shortTargets: [{ price: 110, percentage: 0.1 }]
+                    boxup: 150, boxdown: 148,
+                    longTargets: [{ price: 153, percentage: 0.1 }],
+                    shortTargets: [{ price: 145, percentage: 0.1 }]
                 },
                 'BABA': {
                     highQualityNews: 'china ease lockdown', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                    deferTradingInSeconds: 80,
+                    deferTradingInSeconds: 0,
                     initialBias: 0,
                     //boxup: 80.75, boxdown: 79.5,
                     longTargets: [],
                     shortTargets: [{ price: 79.1, percentage: 0.1 }]
                 },
-                'AMZN': {
-                    highQualityNews: 'announce best black friday', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                'TSLA': {
+                    highQualityNews: 'china ev delivery numbers', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                     deferTradingInSeconds: 0,
                     initialBias: 0,
-                    //boxup: 186, boxdown: 184,
-                    longTargets: [{ price: 93.95, percentage: 0.1 }],
+                    boxup: 199, boxdown: 196,
+                    longTargets: [{ price: 200, percentage: 0.1 }],
                     shortTargets: []
                 },
                 'QQQ': {
