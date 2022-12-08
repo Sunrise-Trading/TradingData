0be153f2edce05ece32207b9f7f5b8c385d046c9
Rong Ling <wumnlr@gmail.com>
2022-12-08 06:13:39 -0800

    a

--- public/watchlist/historicalData.js
+++ public/watchlist/historicalData.js
@@ -2,6 +2,70 @@ export const TradingData = {
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
+        '12/7/2022': {
+            'stocks': [
+                'AAPL',
+                'TSLA',
+                'MDB',
+                'CVNA'
+            ],
+            'StockCandidates': {
+                'CVNA': {
+                    highQualityNews: 'wedbush cut price from $9 to $1', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 120,
+                    initialBias: 0,
+                    boxup: 90, boxdown: 88,
+                    longTargets: [],
+                    shortTargets: [{ price: 89, percentage: 0.1 }]
+                },
+                'MDB': {
+                    highQualityNews: 'earnings beat', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 183.5, boxdown: 181,
+                    longTargets: [],
+                    shortTargets: [{ price: 173.5, percentage: 0.2 }]
+                },
+                'AAPL': {
+                    highQualityNews: 'morgan stanley cuts iphone estimates', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 54.5, boxdown: 53,
+                    longTargets: [{ price: 153, percentage: 0.1 }],
+                    shortTargets: [{ price: 145, percentage: 0.1 }]
+                },
+                'BABA': {
+                    highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 94.5, boxdown: 92.5,
+                    longTargets: [],
+                    shortTargets: [{ price: 92, percentage: 0.1 }],
+                },
+                'TSLA': {
+                    highQualityNews: 'Labor Disaster At Giga Berlin', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 177, boxdown: 173,
+                    longTargets: [],
+                    shortTargets: [{ price: 186, percentage: 0.1 }]
+                },
+                'QQQ': {
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longTargets: [{ price: 384.8, percentage: 0.2 }],
+                    shortTargets: []
+                },
+                'SPY': {
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longTargets: [{ price: 384.8, percentage: 0.2 }],
+                    shortTargets: []
+                },
+            }
+        },
         '12/5/2022': {
             'StockSelection': {
                 'index': ['QQQ', 'SPY'],
--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -7,35 +7,27 @@ window.TradingData = {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
         'stocks': [
-            'AAPL',
             'TSLA',
-            'MDB',
-            'CVNA'
+            'META',
+            'AAPL',
+            'BABA'
         ],
         'StockCandidates': {
-            'CVNA': {
-                highQualityNews: 'wedbush cut price from $9 to $1', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                deferTradingInSeconds: 120,
+            'AAPL': {
+                highQualityNews: 'foxconn covid lift', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                deferTradingInSeconds: 0,
                 initialBias: 0,
                 boxup: 90, boxdown: 88,
                 longTargets: [],
                 shortTargets: [{ price: 89, percentage: 0.1 }]
             },
-            'MDB': {
-                highQualityNews: 'earnings beat', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+            'META': {
+                highQualityNews: 'state restriction on tiktok', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 boxup: 183.5, boxdown: 181,
                 longTargets: [],
                 shortTargets: [{ price: 173.5, percentage: 0.2 }]
-            },
-            'AAPL': {
-                highQualityNews: 'morgan stanley cuts iphone estimates', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                deferTradingInSeconds: 0,
-                initialBias: 0,
-                boxup: 54.5, boxdown: 53,
-                longTargets: [{ price: 153, percentage: 0.1 }],
-                shortTargets: [{ price: 145, percentage: 0.1 }]
             },
             'BABA': {
                 highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
@@ -46,12 +38,20 @@ window.TradingData = {
                 shortTargets: [{ price: 92, percentage: 0.1 }],
             },
             'TSLA': {
-                highQualityNews: 'Labor Disaster At Giga Berlin', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                highQualityNews: 'shorten shanghai factory shifts', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 boxup: 177, boxdown: 173,
                 longTargets: [],
                 shortTargets: [{ price: 186, percentage: 0.1 }]
+            },
+            'PDD': {
+                highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                deferTradingInSeconds: 0,
+                initialBias: 0,
+                boxup: 94.5, boxdown: 92.5,
+                longTargets: [],
+                shortTargets: [{ price: 92, percentage: 0.1 }],
             },
             'QQQ': {
                 highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
