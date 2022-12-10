d1753986f0a72f32205c96cf88f662fa6f0df145
Rong Ling <wumnlr@gmail.com>
2022-12-10 09:48:38 -0800

    a

--- public/watchlist/historicalData.js
+++ public/watchlist/historicalData.js
@@ -2,6 +2,70 @@ export const TradingData = {
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
+        '12/9/2022': {
+            'stocks': [
+                'BABA',
+                'TSLA',
+                'QQQ',
+                'SPY',
+            ],
+            'StockCandidates': {
+                'AAPL': {
+                    highQualityNews: 'foxconn covid lift', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 90, boxdown: 88,
+                    longTargets: [],
+                    shortTargets: [{ price: 89, percentage: 0.1 }]
+                },
+                'META': {
+                    highQualityNews: 'state restriction on tiktok', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 183.5, boxdown: 181,
+                    longTargets: [],
+                    shortTargets: [{ price: 173.5, percentage: 0.2 }]
+                },
+                'BABA': {
+                    highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    //boxup: 94.5, boxdown: 92.5,
+                    longTargets: [],
+                    shortTargets: [{ price: 92.92, percentage: 0.1 }],
+                },
+                'TSLA': {
+                    highQualityNews: 'suspend model Y in shanghai', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 175, boxdown: 171.5,
+                    longTargets: [],
+                    shortTargets: [{ price: 171, percentage: 0.1 }]
+                },
+                'PDD': {
+                    highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 88.75, boxdown: 87.75,
+                    longTargets: [],
+                    shortTargets: [{ price: 92, percentage: 0.1 }],
+                },
+                'QQQ': {
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longTargets: [{ price: 384.8, percentage: 0.2 }],
+                    shortTargets: []
+                },
+                'SPY': {
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longTargets: [{ price: 384.8, percentage: 0.2 }],
+                    shortTargets: []
+                },
+            }
+        },
         '12/8/2022': {
             'stocks': [
                 'META',
