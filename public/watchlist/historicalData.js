c6c0e880d7720b2620e2ff686ac383043950e4da
Rong Ling <wumnlr@gmail.com>
2022-12-12 21:35:46 -0800

    update

--- public/watchlist/historicalData.js
+++ public/watchlist/historicalData.js
@@ -2,6 +2,69 @@ export const TradingData = {
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
+        '12/12/2022': {
+            'stocks': [
+                'TSLA',
+                'RIVN',
+                'QQQ',
+            ],
+            'StockCandidates': {
+                'RIVN': {
+                    highQualityNews: 'pause plans of EV van', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    //boxup: 90, boxdown: 88,
+                    longTargets: [],
+                    shortTargets: [{ price: 89, percentage: 0.1 }]
+                },
+                'AAPL': {
+                    highQualityNews: 'aapl pay and supply news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 183.5, boxdown: 181,
+                    longTargets: [],
+                    shortTargets: [{ price: 173.5, percentage: 0.2 }]
+                },
+                'BABA': {
+                    highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    //boxup: 94.5, boxdown: 92.5,
+                    longTargets: [],
+                    shortTargets: [{ price: 92.92, percentage: 0.1 }],
+                },
+                'TSLA': {
+                    highQualityNews: 'rivn news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 178, boxdown: 176,
+                    longTargets: [],
+                    shortTargets: [{ price: 175.3, percentage: 0.1 }]
+                },
+                'PDD': {
+                    highQualityNews: 'china news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 88.75, boxdown: 87.75,
+                    longTargets: [],
+                    shortTargets: [{ price: 92, percentage: 0.1 }],
+                },
+                'QQQ': {
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longTargets: [{ price: 384.8, percentage: 0.2 }],
+                    shortTargets: []
+                },
+                'SPY': {
+                    highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longTargets: [{ price: 384.8, percentage: 0.2 }],
+                    shortTargets: []
+                },
+            }
+        },
         '12/9/2022': {
             'stocks': [
                 'BABA',
--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -1,7 +1,7 @@
 window.TradingData = {
     'Settings': {
         'currentDayStr': '', //'2022-12-2 6:30'
-        'activeProfileName': "momentumEquities", //choppyMarket"; //"momentumEquities";
+        'activeProfileName': "futures", //choppyMarket"; //"momentumEquities";
     },
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
