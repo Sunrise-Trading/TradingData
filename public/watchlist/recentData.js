c3720ac73a8a1d483de7a07e125e836e2df93856
Rong Ling <wumnlr@gmail.com>
2022-12-16 06:12:06 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -7,12 +7,14 @@ window.TradingData = {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESZ22'],
         'stocks': [
+            'META',
+            'TSLA',
             'SPY',
             'QQQ',
         ],
         'StockCandidates': {
-            'AAPL': {
-                highQualityNews: 'aapl pay and supply news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+            'META': {
+                highQualityNews: 'upgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 boxup: 183.5, boxdown: 181,
@@ -20,7 +22,7 @@ window.TradingData = {
                 shortTargets: [{ price: 173.5, percentage: 0.2 }]
             },
             'TSLA': {
-                highQualityNews: 'rivn news', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                highQualityNews: 'elon done selling', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 boxup: 160, boxdown: 157.5,
@@ -28,7 +30,7 @@ window.TradingData = {
                 shortTargets: [{ price: 175.3, percentage: 0.1 }]
             },
             'QQQ': {
-                highQualityNews: 'data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                highQualityNews: 'post fomc', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 longTargets: [{ price: 384.8, percentage: 0.2 }],
