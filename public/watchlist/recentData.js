8136cb9cd583e5e6b0e7714275dbd1ed6d3e8638
Rong Ling <wumnlr@gmail.com>
2022-12-22 06:22:39 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -9,57 +9,50 @@ window.TradingData = {
         'checklistKeys': ["Read news carefully", "Review 1 minute chart", "Review hourly chart", "Review daily chart", "Review weekly chart ", "Draw nearest price levels", "Check whether it gap up/down above/below support/resistance"],
         'checklist': {
             'TSLA': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
-            'NKE': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+            'NVDA': {
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'QQQ': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'SPY': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
         },
         'stocks': [
             'TSLA',
+            'NVDA',
             'QQQ',
             'SPY',
         ],
         'StockCandidates': {
-            'NKE': {
-                highQualityNews: 'earnings beat', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+            'NVDA': {
+                highQualityNews: 'MU earnings miss', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
-                boxup: 117, boxdown: 115,
-                longTargets: [{ price: 119.5, percentage: 0.1 }],
-                shortTargets: [{ price: 110.5, percentage: 0.1 }]
-            },
-            'AMZN': {
-                highQualityNews: 'EU approval', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
-                deferTradingInSeconds: 0,
-                initialBias: 0,
-                //boxup: 119, boxdown: 181,
+                boxup: 162, boxdown: 160.5,
                 longTargets: [],
-                shortTargets: [{ price: 116.83, percentage: 0.1 }]
+                shortTargets: [{ price: 157, percentage: 0.1 }]
             },
             'TSLA': {
-                highQualityNews: 'freeze hiring', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                highQualityNews: 'double discount', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
-                boxup: 141, boxdown: 138,
+                boxup: 137, boxdown: 134.5,
                 longTargets: [],
                 shortTargets: [{ price: 135, percentage: 0.1 }]
             },
