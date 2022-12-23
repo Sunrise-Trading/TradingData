126137b0c6e30eba37fdf35750faa5d85bcd53fe
Rong Ling <wumnlr@gmail.com>
2022-12-23 06:26:06 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -14,7 +14,7 @@ window.TradingData = {
                 "Review weekly chart ": true, "Draw nearest price levels": true,
                 "Check whether it gap up/down above/below support/resistance": true,
             },
-            'NVDA': {
+            'META': {
                 "Read news carefully": true, "Review 1 minute chart": true,
                 "Review hourly chart": true, "Review daily chart": true,
                 "Review weekly chart ": true, "Draw nearest price levels": true,
