c8a7a44bddbb1431f5a84f25c753784cf679a1ab
Rong Ling <wumnlr@gmail.com>
2022-12-30 06:18:39 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -9,43 +9,44 @@ window.TradingData = {
         'checklistKeys': ["Read news carefully", "Review 1 minute chart", "Review hourly chart", "Review daily chart", "Review weekly chart ", "Draw nearest price levels", "Check whether it gap up/down above/below support/resistance"],
         'checklist': {
             'TSLA': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
-            'META': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+            'FUTU': {
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'QQQ': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'SPY': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
         },
         'stocks': [
             'TSLA',
+            'FUTU',
             'QQQ',
             'SPY',
         ],
         'StockCandidates': {
-            'META': {
-                highQualityNews: 'settlement penalty', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+            'FUTU': {
+                highQualityNews: 'china crackdown on illegal activity', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
-                boxup: 162, boxdown: 160.5,
+                boxup: 46, boxdown: 41,
                 longTargets: [],
-                shortTargets: [{ price: 157, percentage: 0.1 }]
+                shortTargets: []
             },
             'TSLA': {
                 highQualityNews: 'Elon tells employee', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
