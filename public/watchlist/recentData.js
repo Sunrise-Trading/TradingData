931da866a5129ff1f197840fe36b4765b18c5eae
Rong Ling <wumnlr@gmail.com>
2023-01-03 06:19:23 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -9,39 +9,39 @@ window.TradingData = {
         'checklistKeys': ["Read news carefully", "Review 1 minute chart", "Review hourly chart", "Review daily chart", "Review weekly chart ", "Draw nearest price levels", "Check whether it gap up/down above/below support/resistance"],
         'checklist': {
             'TSLA': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'FUTU': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'QQQ': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
             'SPY': {
-                "Read news carefully": false, "Review 1 minute chart": false,
-                "Review hourly chart": false, "Review daily chart": false,
-                "Review weekly chart ": false, "Draw nearest price levels": false,
-                "Check whether it gap up/down above/below support/resistance": false,
+                "Read news carefully": true, "Review 1 minute chart": true,
+                "Review hourly chart": true, "Review daily chart": true,
+                "Review weekly chart ": true, "Draw nearest price levels": true,
+                "Check whether it gap up/down above/below support/resistance": true,
             },
         },
         'stocks': [
             'TSLA',
-            'FUTU',
-            'QQQ',
+            'AAPL',
+            'BABA',
             'SPY',
         ],
         'StockCandidates': {
-            'FUTU': {
-                highQualityNews: 'china crackdown on illegal activity', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+            'AAPL': {
+                highQualityNews: 'foxconn back to production', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 boxup: 46, boxdown: 41,
@@ -49,15 +49,15 @@ window.TradingData = {
                 shortTargets: []
             },
             'TSLA': {
-                highQualityNews: 'Elon tells employee', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                highQualityNews: 'delivery miss estimate', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
-                boxup: 121, boxdown: 118,
+                boxup: 120.5, boxdown: 116.5,
                 longTargets: [],
-                shortTargets: [{ price: 135, percentage: 0.1 }]
+                shortTargets: []
             },
-            'QQQ': {
-                highQualityNews: 'jobless data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+            'BABA': {
+                highQualityNews: 'china reopen', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 longTargets: [],
