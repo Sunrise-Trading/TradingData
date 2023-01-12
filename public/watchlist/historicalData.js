f27c36c4d6f1acc8ee4a88281edec2dc7959c8ea
Rong Ling <wumnlr@gmail.com>
2023-01-11 20:41:24 -0800

    a

--- public/watchlist/historicalData.js
+++ public/watchlist/historicalData.js
@@ -2,6 +2,64 @@ export const TradingData = {
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESH23'],
+        '1/11/2023': {
+            'stocks': [
+                'COIN',
+                'TSLA',
+                'QQQ',
+                'SPY',
+            ],
+            'StockCandidates': {
+                'TSLA': {
+                    highQualityNews: 'expand factory', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 122.5, boxdown: 110.5,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [123.52, 122.76, 118.46, 120, 116, 114, 104.64, 107.16, 108.24],
+                    longTargets: [],
+                    shortTargets: []
+                },
+                'COIN': {
+                    highQualityNews: 'downgrade and close japan', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: -1,
+                    boxup: 42.5, boxdown: 41.25,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 44,
+                    keyLevels: [42.4, 41.2, 40.75, 40],
+                    longTargets: [],
+                    shortTargets: []
+                },
+                'BABA': {
+                    highQualityNews: 'upgrade', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 114, boxdown: 112,
+                    longOnlyIfAbovePrice: 111.98, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [112, 110],
+                    longTargets: [],
+                    shortTargets: []
+                },
+                'QQQ': {
+                    highQualityNews: 'job data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [272, 264.89, 263.8],
+                    longTargets: [],
+                    shortTargets: []
+                },
+                'SPY': {
+                    highQualityNews: 'jobless data', volumeSum: 0, tradingSum: 0, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [393.7, 389.61, 381.84, 383.77, 384.5],
+                    longTargets: [],
+                    shortTargets: []
+                },
+            }
+        },
         '1/10/2023': {
             'stocks': [
                 'TSLA',
