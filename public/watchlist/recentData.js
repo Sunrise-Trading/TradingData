da575a83bcd1374eac64a9ea0f27966fc40abcbc
Rong Ling <wumnlr@gmail.com>
2023-01-21 08:48:10 -0800

    a

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -2,6 +2,7 @@ window.TradingData = {
     'Settings': {
         'currentDayStr': '', // 2023-12-2 6:30
         'activeProfileName': "momentumSimple",  // momentumSimple, momentumEquities, momentumScalp, fomc;
+        'systemProfileName': 'dt', // testTDA
     },
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
