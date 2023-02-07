003b12c5d7c7422bfa56360faf9c250bd2d210ce
Rong Ling <wumnlr@gmail.com>
2023-02-06 21:01:50 -0800

    a

--- public/watchlist/historicalData.js
+++ public/watchlist/historicalData.js
@@ -2,6 +2,66 @@ export const TradingData = {
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESH23'],
+        '2/6/2023': {
+            'stocks': [
+                'BABA', 'TSLA', 'SPY', 'QQQ'
+            ],
+            'StockCandidates': {
+                'META': {
+                    highQualityNews: 'earnings buy back', volumeSum: 2372653, tradingSum: 433422288, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 185, boxdown: 182,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [181.71],
+                    longTargets: [],
+                    shortTargets: [],
+                    tradingPlans: ['s-pop', 'b-pullback', 's-breakout', 'b-breakout'],
+                },
+                'BABA': {
+                    highQualityNews: 'SPY ballon', volumeSum: 132841, tradingSum: 13860544, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    //boxup: 127.5, boxdown: 125,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [91.4],
+                    longTargets: [],
+                    shortTargets: [],
+                    tradingPlans: ['s-pop', 's-pullback', 's-breakout'],
+                },
+                'TSLA': {
+                    highQualityNews: 'earnings', volumeSum: 3156840, tradingSum: 610480141, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    boxup: 127.5, boxdown: 125,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [160, 156, 152.5, 150],
+                    longTargets: [],
+                    shortTargets: [],
+                    tradingPlans: ['b-open', 'b-dip', 'b-pullback', 'b-breakout'],
+                },
+                'QQQ': {
+                    highQualityNews: 'data', volumeSum: 1349739, tradingSum: 409453284, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [303.43, 304.5],
+                    longTargets: [],
+                    shortTargets: [],
+                    tradingPlans: ['b-open', 's-pullback', 's-pop', 's-breakout'],
+                },
+                'SPY': {
+                    highQualityNews: 'data', volumeSum: 843282, tradingSum: 345315204, premktHigh: 0, premktLow: 99999999,
+                    deferTradingInSeconds: 0,
+                    initialBias: 0,
+                    longOnlyIfAbovePrice: 0, shortOnlyIfBelowPrice: 0,
+                    keyLevels: [413.67],
+                    longTargets: [],
+                    shortTargets: [],
+                    tradingPlans: ['b-open', 's-pullback', 's-pop', 's-breakout'],
+                },
+            }
+        },
         '2/2/2023': {
             'stocks': [
                 'META', 'SPY', 'QQQ'
