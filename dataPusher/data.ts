3cb992544a7d61049c4435cf00538fcdc343d8f7
Rong Ling <wumnlr@gmail.com>
2023-03-13 06:22:02 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -78,14 +78,16 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         dailyRange: 4,
         fixQuantity: 100,
         keyLevels: {
-            lastDefenseForLong: 296.37,
-            lastDefenseForShort: 299.1,
-            otherLevels: [298.43],
+            lastDefenseForLong: 285,
+            lastDefenseForShort: 290,
+            otherLevels: [287.38],
         },
         long: {
+            openingDrive: { enabled: true },
             retracement: {
                 entryAreas: [
-                    { priceLevel: 296.7, upperRoom: 0.5, lowerRoom: 0.5 },
+                    { priceLevel: 287.38, upperRoom: 0.5, lowerRoom: 0.5 },
+                    { priceLevel: 284.73, upperRoom: 0.5, lowerRoom: 0.5 },
                 ],
                 allowAgainstVwap: true,
                 vwapArea: { upperRoom: 0.7, lowerRoom: 0.7 },
@@ -94,13 +96,10 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 breakoutLevels: [299.2],
             }
         },
-        short: {
-            openingDrive: { enabled: true },
-
-        },
+        short: {},
     },
     {
-        symbol: 'MESH23',
+        symbol: 'MESM23',
         dailyRange: 40,
         fixQuantity: 2,
         keyLevels: {
