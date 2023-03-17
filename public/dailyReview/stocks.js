4cd96b990588684126b0076824d68aef14df961d
Rong Ling <wumnlr@gmail.com>
2023-03-16 21:31:35 -0700

    add watchlist

--- public/dailyReview/stocks.js
+++ public/dailyReview/stocks.js
@@ -1,6 +1,7 @@
 let stocks = [
     /* index */
     'SPY', 'QQQ', 'DIA', 'IWM', 'VIX',
+    '/ES', '/NQ', '/YM', '/RTY',
     // SP 500 sectors
     "XLY", // Cyclicals
     "XLK", // Technology
@@ -13,4 +14,12 @@ let stocks = [
     "XLF", // Financials
     /* big tech */
     'AAPL', 'MSFT', 'AMZN', 'META', 'TSLA', 'NFLX', 'NVDA', 'GOOGL',
+    /* chips */
+    'NVDA', 'AMD', 'MU', 'INTC',
+    /* finance */
+    'JPM', 'BAC', 'SCHW',
+    /* regional banks */
+    'KRE', 'FRC',
+    /* gig economy */
+    'UBER', 'LYFT', 'DASH', 'ABNB',
 ];
