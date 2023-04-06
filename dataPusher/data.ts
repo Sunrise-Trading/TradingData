9609048456356bdf9d322c9410c77937706f1c8e
Rong Ling <wumnlr@gmail.com>
2023-04-06 06:17:33 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -61,33 +61,23 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     },
     {
         symbol: 'TSLA',
-        dailyRange: 7,
+        dailyRange: 8,
         fixQuantity: 100,
         keyLevels: {
             otherLevels: [208],
         },
-        long: {
-            openingDrive: { lastDefense: 199.50 },
-            retracement: {
-                entryAreas: [
-                    { priceLevel: 199.6, upperRoom: 1, lowerRoom: 1 },
-                    { priceLevel: 197.5, upperRoom: 1, lowerRoom: 1 },
-                ],
-                allowAgainstVwap: false,
-                lastDefense: 196.5,
-            }
-        },
+        long: {},
         short: {
             momentum: {
-                breakoutLevels: [202],
+                breakoutLevels: [184],
             },
             retracement: {
                 entryAreas: [
-                    { priceLevel: 204.6, upperRoom: 1, lowerRoom: 1 },
-                    { priceLevel: 202.86, upperRoom: 1, lowerRoom: 1 },
+                    { priceLevel: 184, upperRoom: 1, lowerRoom: 1 },
+                    { priceLevel: 185, upperRoom: 1, lowerRoom: 1 },
                 ],
                 allowAgainstVwap: true,
-                lastDefense: 205,
+                lastDefense: 185.76,
             }
         },
     },
--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -15,7 +15,7 @@ window.TradingData = {
                 "Review weekly chart ": true, "Draw nearest price levels": true,
                 "Check whether it gap up/down above/below support/resistance": true,
             },
-            'GOOGL': {
+            'COST': {
                 "Read news carefully": true, "Review 1 minute chart": true,
                 "Review hourly chart": true, "Review daily chart": true,
                 "Review weekly chart ": true, "Draw nearest price levels": true,
@@ -35,11 +35,11 @@ window.TradingData = {
             },
         },
         'stocks': [
-            'GOOGL', 'SPY', 'QQQ'
+            'TSLA',
         ],
         'StockCandidates': {
             'TSLA': {
-                highQualityNews: 'delivery numbers no good', volumeSum: 936575, tradingSum: 180261435, premktHigh: 0, premktLow: 99999999,
+                highQualityNews: 'master plan', volumeSum: 1619608, tradingSum: 297583167, premktHigh: 0, premktLow: 99999999,
                 deferTradingInSeconds: 0,
                 initialBias: 0,
                 longTargets: [],
