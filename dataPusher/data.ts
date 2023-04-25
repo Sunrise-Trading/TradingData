905953d37c66a1d235e18988a4e226691fbaaa8f
Rong Ling <wumnlr@gmail.com>
2023-04-25 06:16:16 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -1,27 +1,56 @@
 import type * as TradingPlans from './models';
 
-export const stockSelections: string[] = ['TSLA'];
+export const stockSelections: string[] = ['FRC', 'SPOT', 'GM'];
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'TSLA',
-        vwapCorrection: { volumeSum: 4091860, tradingSum: 682650298 },
-        dailyRange: 5,
-        fixQuantity: 60,
+        symbol: 'FRC',
+        vwapCorrection: { volumeSum: 4716172, tradingSum: 58236470 },
+        dailyRange: 3,
+        fixQuantity: 500,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [163.91] },
-        short: {},
+        keyLevels: { otherLevels: [11.52] },
+        short: {
+            momentum: { breakoutLevels: [12.3] },
+        },
         long: {
+            openingDrive: { lastDefense: 11.75 },
+            momentum: {
+                breakoutLevels: [12.41],
+            }
+        },
+    },
+    {
+        symbol: 'GM',
+        vwapCorrection: { volumeSum: 597963, tradingSum: 21060764 },
+        dailyRange: 1,
+        fixQuantity: 500,
+        alwaysUseFixQuantity: false,
+        keyLevels: { otherLevels: [35.8] },
+        short: {
+            openingDrive: { lastDefense: 35.5 },
+            momentum: { breakoutLevels: [35.3] },
             retracement: {
                 entryAreas: [
-                    { priceLevel: 165, upperRoom: 1.5, lowerRoom: 1.5 },
+                    { priceLevel: 35.3, upperRoom: 0.15, lowerRoom: 0.15 },
                 ],
                 allowAgainstVwap: false,
-                lastDefense: 163.91,
-            },
-            openingDrive: { lastDefense: 163.91 },
-            momentum: {
-                breakoutLevels: [163.91],
+                lastDefense: 35.5,
             }
+        },
+        long: {},
+    },
+    {
+        symbol: 'SPOT',
+        vwapCorrection: { volumeSum: 200602, tradingSum: 27558684 },
+        dailyRange: 3.5,
+        fixQuantity: 100,
+        alwaysUseFixQuantity: false,
+        keyLevels: { otherLevels: [136.73] },
+        short: {
+            openingDrive: { lastDefense: 142.48 },
+        },
+        long: {
+            momentum: { breakoutLevels: [136.73] },
         },
     },
     {
