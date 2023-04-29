aabb4dcf9eb4cdf55ad341a58444faecf63dc2b3
Rong Ling <wumnlr@gmail.com>
2023-04-29 08:30:47 -0700

    remove dependency on watchlist item

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -7,28 +7,5 @@ window.TradingData = {
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
         'futures': ['MESM23', 'MNQM23'],
-        'checklistKeys': ["Read news carefully", "Review 1 minute chart", "Review hourly chart", "Review daily chart", "Review weekly chart ", "Draw nearest price levels", "Check whether it gap up/down above/below support/resistance"],
-        'StockCandidates': {
-            'TSLA': {
-                longTargets: [],
-                shortTargets: [],
-            },
-            'FRC': {
-                longTargets: [],
-                shortTargets: [],
-            },
-            'META': {
-                longTargets: [],
-                shortTargets: [],
-            },
-            'SPOT': {
-                longTargets: [],
-                shortTargets: [],
-            },
-            'SPY': {
-                longTargets: [],
-                shortTargets: [],
-            },
-        }
     }
 };
