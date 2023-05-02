93b93ea4236134f2862748b2ba2df216f14415ab
Rong Ling <wumnlr@gmail.com>
2023-05-02 06:11:34 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -1,19 +1,19 @@
 import type * as TradingPlans from './models';
 
-export const stockSelections: string[] = ['JPM'];
+export const stockSelections: string[] = ['TSLA'];
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'JPM',
-        vwapCorrection: { volumeSum: 459018, tradingSum: 65857695 },
-        dailyRange: 2.5,
+        symbol: 'TSLA',
+        vwapCorrection: { volumeSum: 497845, tradingSum: 80561275 },
+        dailyRange: 7,
         fixQuantity: 100,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [144.34] },
-        short: {
+        keyLevels: { otherLevels: [162.66] },
+        long: {
             openingDrive: {
-                lastDefense: 144.34,
+                lastDefense: 161.5,
                 targets: {
-                    priceLevels: [142],
+                    priceLevels: [162.5],
                     rrr: [1.5],
                 }
             },
@@ -26,55 +26,49 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             retracement: {
                 entryAreas: [
                     {
-                        priceArea: { priceLevel: 145, upperRoom: 0.2, lowerRoom: 0.6 },
+                        priceArea: { priceLevel: 160.84, upperRoom: 0.25, lowerRoom: 0.25 },
                         targets: {
-                            priceLevels: [142],
+                            priceLevels: [161.5],
+                            rrr: [1.5],
+                        }
+                    },
+                    {
+                        priceArea: { priceLevel: 161.62, upperRoom: 0.15, lowerRoom: 0.15 },
+                        targets: {
+                            priceLevels: [162.25, 162.5],
                             rrr: [1.5],
                         }
                     }
                 ],
                 allowAgainstVwap: false,
-                lastDefense: 145.2,
+                lastDefense: 160.8,
             }
         },
-        long: {}
+        short: {}
     },
     {
-        symbol: 'META',
-        vwapCorrection: { volumeSum: 2003128, tradingSum: 473658613 },
-        dailyRange: 6,
-        fixQuantity: 50,
+        symbol: 'TLT',
+        vwapCorrection: { volumeSum: 340093, tradingSum: 35389358 },
+        dailyRange: 1,
+        fixQuantity: 500,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [236.15] },
+        keyLevels: { otherLevels: [104.42] },
         short: {
-            openingDrive: {
-                lastDefense: 243,
-                targets: {
-                    priceLevels: [],
-                    rrr: [],
-                },
-            },
             momentum: {
                 targets: {
                     priceLevels: [],
                     rrr: [],
                 },
             },
-            retracement: {
-                entryAreas: [
-                    {
-                        priceArea: { priceLevel: 242.27, upperRoom: 1, lowerRoom: 3 },
-                        targets: {
-                            priceLevels: [],
-                            rrr: [],
-                        },
-                    }
-                ],
-                allowAgainstVwap: true,
-                lastDefense: 243,
-            }
         },
-        long: {},
+        long: {
+            momentum: {
+                targets: {
+                    priceLevels: [],
+                    rrr: [],
+                },
+            },
+        },
     },
     {
         symbol: 'MESM23',
