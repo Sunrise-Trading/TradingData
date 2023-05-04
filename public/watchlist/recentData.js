a7ddf64353024e8bc6e11581cc730ef18a74e4f7
ronglingcode <69086027+ronglingcode@users.noreply.github.com>
2023-05-04 15:14:38 -0700

    deprecate activeProfileName

--- public/watchlist/recentData.js
+++ public/watchlist/recentData.js
@@ -1,11 +1,10 @@
 window.TradingData = {
     'Settings': {
         'currentDayStr': '', // 2023-2-23 6:30
-        'activeProfileName': "momentumSimple",  // momentumSimple, momentumEquities, momentumScalp, fomc;
         'systemProfileName': 'dt', // testTDA
     },
     'StockSelection': {
         'index': ['QQQ', 'SPY'],
-        'futures': ['MESM23', 'MNQM23'],
+        'futures': ['MESM23', 'MNQM23', 'USM23'],
     }
 };
