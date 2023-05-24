7b375e065d1b55f229e703f34f4f95e857830f8d
Rong Ling <wumnlr@gmail.com>
2023-05-24 08:20:57 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -1,46 +1,44 @@
 import * as TradingPlans from './models';
 import * as Constants from './constants';
 export const activeProfileName: string = "momentumSimple";  // futures, momentumSimple, momentumEquities, momentumScalp, fomc;
-export const stockSelections: string[] = ['TSLA', 'SPY', 'QQQ'];
-
-
+export const stockSelections: string[] = ['TSLA', 'PANW', 'SPY', 'QQQ'];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
         symbol: 'TSLA',
-        vwapCorrection: { volumeSum: 825523, tradingSum: 154882485 },
+        vwapCorrection: { volumeSum: 980072, tradingSum: 179483547 },
         dailyRange: 6,
         fixQuantity: 100,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [188] },
+        keyLevels: { otherLevels: [183.8] },
         short: {
-            openingDrive: {
-                lastDefense: 188,
-                stopForAgainstVwapEntry: 188.6,
-                targets: Constants.presetTargets.breakeven,
-                setupQuality: Constants.scalp,
-            },
             momentum: {
                 targets: Constants.presetTargets.breakeven,
                 setupQuality: Constants.scalp,
             },
+        },
+        long: {
             retracement: {
                 entryAreas: [
                     {
                         priceArea: {
-                            priceLevel: 188,
-                            upperRoom: 0.2,
-                            lowerRoom: 0.5
+                            priceLevel: 182,
+                            upperRoom: 1,
+                            lowerRoom: 0.1
                         },
                         targets: Constants.presetTargets.breakeven,
-                        stopPrice: 188.6,
+                        stopPrice: 181.5,
                         setupQuality: Constants.scalp,
                     },
                 ],
-                lastDefense: 188,
-            }
-        },
-        long: {
+                lastDefense: 182,
+            },
+            openingDrive: {
+                lastDefense: 182,
+                stopForAgainstVwapEntry: 181.5,
+                targets: Constants.presetTargets.breakeven,
+                setupQuality: Constants.scalp,
+            },
             momentum: {
                 targets: Constants.presetTargets.breakeven,
                 setupQuality: Constants.scalp,
@@ -48,8 +46,39 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         }
     },
     {
+        symbol: 'PANW',
+        vwapCorrection: { volumeSum: 76093, tradingSum: 15111539 },
+        dailyRange: 4,
+        fixQuantity: 100,
+        alwaysUseFixQuantity: false,
+        keyLevels: { otherLevels: [200] },
+        long: {
+        },
+        short: {
+            momentum: {
+                targets: Constants.presetTargets.r2,
+                setupQuality: Constants.scalp,
+            },
+            retracement: {
+                entryAreas: [
+                    {
+                        priceArea: {
+                            priceLevel: 200,
+                            upperRoom: 0.5,
+                            lowerRoom: 0.5
+                        },
+                        targets: Constants.presetTargets.breakeven,
+                        stopPrice: 202,
+                        setupQuality: Constants.scalp,
+                    },
+                ],
+                lastDefense: 201,
+            },
+        },
+    },
+    {
         symbol: 'SPY',
-        vwapCorrection: { volumeSum: 596534, tradingSum: 249397109 },
+        vwapCorrection: { volumeSum: 1037874, tradingSum: 428533534 },
         dailyRange: 4,
         fixQuantity: 100,
         alwaysUseFixQuantity: false,
@@ -70,7 +99,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     },
     {
         symbol: 'QQQ',
-        vwapCorrection: { volumeSum: 667247, tradingSum: 224817989 },
+        vwapCorrection: { volumeSum: 869619, tradingSum: 288766437 },
         dailyRange: 4,
         fixQuantity: 100,
         alwaysUseFixQuantity: false,
