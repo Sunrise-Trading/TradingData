e7b1e138051a2906ca6592134e2af3763f6262f2
Rong Ling <wumnlr@gmail.com>
2023-05-30 06:18:53 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -1,85 +1,74 @@
 import * as TradingPlans from './models';
 import * as Constants from './constants';
-export const activeProfileName: string = "futures";  // futures, momentumSimple, momentumEquities, momentumScalp, fomc;
-export const stockSelections: string[] = ['PDD', 'MRVL'];
+export const activeProfileName: string = "momentumSimple";  // futures, momentumSimple, momentumEquities, momentumScalp, fomc;
+export const stockSelections: string[] = ['TSLA', 'NVDA', 'AI'];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'PDD',
-        vwapCorrection: { volumeSum: 1314375, tradingSum: 87701251 },
-        dailyRange: 3,
-        fixQuantity: 200,
+        symbol: 'TSLA',
+        vwapCorrection: { volumeSum: 2529437, tradingSum: 506005297 },
+        dailyRange: 7,
+        fixQuantity: 100,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [70] },
+        keyLevels: { otherLevels: [200] },
         short: {
-            openingDrive: {
-                lastDefense: 69.4,
-                stopForAgainstVwapEntry: 69.4,
-                targets: Constants.presetTargets.r2,
-                setupQuality: TradingPlans.SetupQuality.DayTrade,
-            },
-            momentum: {
-                targets: Constants.presetTargets.breakeven,
-                setupQuality: Constants.scalp,
-            },
         },
         long: {
             momentum: {
-                targets: Constants.presetTargets.breakeven,
-                setupQuality: Constants.scalp,
+                targets: Constants.presetTargets.doubleTop,
+                setupQuality: TradingPlans.SetupQuality.HoldToDayClose,
             },
+            openingDrive: {
+                targets: Constants.presetTargets.doubleTop,
+                setupQuality: TradingPlans.SetupQuality.HoldToDayClose,
+                lastDefense: 199.5,
+                stopForAgainstVwapEntry: 199,
+            }
         }
     },
     {
-        symbol: 'MRVL',
-        vwapCorrection: { volumeSum: 964587, tradingSum: 56148315 },
-        dailyRange: 1.5,
-        fixQuantity: 200,
+        symbol: 'NVDA',
+        vwapCorrection: { volumeSum: 1417822, tradingSum: 574690162 },
+        dailyRange: 10,
+        fixQuantity: 100,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [58] },
+        keyLevels: { otherLevels: [400] },
         long: {
-            openingDrive: {
-                lastDefense: 58,
-                stopForAgainstVwapEntry: 57.5,
-                targets: Constants.presetTargets.r2,
-                setupQuality: TradingPlans.SetupQuality.DayTrade,
-            },
             momentum: {
-                targets: Constants.presetTargets.breakeven,
-                setupQuality: Constants.scalp,
+                targets: Constants.presetTargets.doubleTop,
+                setupQuality: TradingPlans.SetupQuality.HoldToDayClose,
             },
+            openingDrive: {
+                targets: Constants.presetTargets.doubleTop,
+                setupQuality: TradingPlans.SetupQuality.HoldToDayClose,
+                lastDefense: 403,
+                stopForAgainstVwapEntry: 401,
+            }
         },
         short: {
-            openingDrive: {
-                lastDefense: 58.5,
-                stopForAgainstVwapEntry: 58.6,
-                targets: Constants.presetTargets.r2,
-                setupQuality: TradingPlans.SetupQuality.DayTrade,
-            },
-            momentum: {
-                targets: Constants.presetTargets.breakeven,
-                setupQuality: Constants.scalp,
-            },
+
         },
     },
     {
-        symbol: 'SPY',
-        vwapCorrection: { volumeSum: 1037874, tradingSum: 428533534 },
-        dailyRange: 4,
-        fixQuantity: 100,
+        symbol: 'AI',
+        vwapCorrection: { volumeSum: 2125902, tradingSum: 77284146 },
+        dailyRange: 3,
+        fixQuantity: 200,
         alwaysUseFixQuantity: false,
-        keyLevels: { otherLevels: [66.43, 65.08] },
+        keyLevels: { otherLevels: [34.68] },
         long: {
+            openingDrive: {
+                targets: Constants.presetTargets.doubleTop,
+                setupQuality: TradingPlans.SetupQuality.HoldToDayClose,
+                lastDefense: 36,
+                stopForAgainstVwapEntry: 35.5
+            },
             momentum: {
-                targets: Constants.presetTargets.r2,
-                setupQuality: Constants.scalp,
+                targets: Constants.presetTargets.doubleTop,
+                setupQuality: TradingPlans.SetupQuality.HoldToDayClose,
             }
         },
         short: {
-            momentum: {
-                targets: Constants.presetTargets.r2,
-                setupQuality: Constants.scalp,
-            }
 
         },
     },
