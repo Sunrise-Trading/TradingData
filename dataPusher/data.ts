1a80adc14d54365f7cbdda3e3c1380cbd7ab8aeb
Niko <63621856+niko-2020@users.noreply.github.com>
2023-08-29 06:11:51 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -17,11 +17,11 @@ const longTarget: TradingPlans.ExitTargets = {
     }
 };
 const shortTarget: TradingPlans.ExitTargets = {
-    priceLevels: [232.25],
+    priceLevels: [88.35, 87.8],
     rrr: [0.8, 1, 1.8, 1.9, 1.95],
     dailyRanges: [],
     minimumTargets: {
-        rrr: [],
+        rrr: [0.8, 0.8, 1, 1, 1.5, 1.8, 2, 2, 3, 3.5],
         priceLevels: [],
         dailyRanges: [],
     }
@@ -47,7 +47,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         fixQuantity: 100,
         alwaysUseFixQuantity: false,
         keyLevels: {
-            otherLevels: [],
+            otherLevels: [89.88],
             momentumStartForLong: 90,
             momentumStartForShort: 92,
         },
@@ -75,8 +75,8 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 planType: TradingPlans.PlanType.OpeningDrive,
                 targets: longTarget,
                 planConfigs: Constants.scalpConfig,
-                lastDefense: 230,
-                stopForAgainstVwapLimitOrMarketEntry: 229,
+                lastDefense: 90,
+                stopForAgainstVwapLimitOrMarketEntry: 89.87,
             },
         }
     },
