8b72d73457dffac0ac112edb72270f1204e275c5
Rong Ling <wumnlr@gmail.com>
2023-10-20 06:22:53 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -5,90 +5,42 @@ export const activeProfileName: string = "momentumSimple";  // futures, momentum
 export const tradingSettigns: TradingPlans.TradingSettings = {
     equalWeightDivider: 3,
 };
-const tslaTarget: TradingPlans.ExitTargets = {
-    initialTargets: {
-        priceLevels: [],
-        rrr: [0.9, 0.95, 1.5, 1.8, 1.9, 2, 2, 3, 3, 5],
-        dailyRanges: [0.8, 0.8, 0.8, 0.8, 0.8, 0.9, 0.9, 1, 1, 1],
-    },
-    minimumTargets: {
-        priceLevels: [],
-        rrr: [0.9, 0.95, 1, 1.6, 1.8, 1.8, 1.8, 2, 2.5, 3],
-        dailyRanges: [0.4, 0.4, 0.45, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-    }
-}
 
-const nflxTarget: TradingPlans.ExitTargets = {
+const sedg: TradingPlans.ExitTargets = {
     initialTargets: {
-        priceLevels: [],
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 71, 71],
         rrr: [0.9, 0.95, 1.8, 1.9, 2, 3, 3.5, 4, 5, 10],
         dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
     },
     minimumTargets: {
         rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 2, 2, 2, 4],
-        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 71, 71],
         dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
     }
 }
 export const stockSelections: string[] = [
-    'NFLX',
-    'TSLA',
-
+    'SEDG',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'NFLX',
-        vwapCorrection: { volumeSum: 568981, tradingSum: 225342085 },
-        dailyRange: 12,
+        symbol: 'SEDG',
+        vwapCorrection: { volumeSum: 680646, tradingSum: 55544291 },
+        dailyRange: 9,
         deferTradingInSeconds: 0,
         keyLevels: {
-            momentumStartForLong: 395,
-            momentumStartForShort: 400,
-        },
-        short: {
-            /*
-            openingDrive: {
-                targets: nflxTarget,
-                stopForAgainstVwapLimitOrMarketEntry: 400,
-                lastDefense: 400
-            },*/
-            momentum: { targets: nflxTarget },
-        },
-        long: {
-            openingDrive: {
-                targets: nflxTarget,
-                stopForAgainstVwapLimitOrMarketEntry: 398,
-                lastDefense: 398
-            },
-            momentum: { targets: nflxTarget },
-        }
-    },
-    {
-        symbol: 'TSLA',
-        vwapCorrection: { volumeSum: 3348743, tradingSum: 760817561 },
-        dailyRange: 10,
-        deferTradingInSeconds: 0,
-        keyLevels: {
-            momentumStartForLong: 226,
-            momentumStartForShort: 230,
+            momentumStartForLong: 88,
+            momentumStartForShort: 87,
         },
         short: {
             openingDrive: {
-                targets: tslaTarget,
-                stopForAgainstVwapLimitOrMarketEntry: 230,
-                lastDefense: 230
+                targets: sedg,
+                stopForAgainstVwapLimitOrMarketEntry: 88,
+                lastDefense: 88
             },
-            momentum: { targets: tslaTarget },
+            momentum: { targets: sedg },
         },
         long: {
-            /*
-            openingDrive: {
-                targets: tslaTarget,
-                stopForAgainstVwapLimitOrMarketEntry: 224,
-                lastDefense: 224
-            },*/
-            momentum: { targets: tslaTarget },
         }
     },
     /*
