e6fa64d8a8643882d059ff81a7f26fbbb78e85e7
Rong Ling <wumnlr@gmail.com>
2023-10-24 07:54:34 -0700

    j

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -9,12 +9,12 @@ const smallTarget: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         rrr: [0.9, 0.95, 1.8, 1.9, 2, 3, 3.5, 4, 5, 10],
-        dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
+        dailyRanges: [0.4, 0.45, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1],
     },
     minimumTargets: {
         rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 2, 2, 2, 2],
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
+        dailyRanges: [0.4, 0.45, 0.45, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
     }
 }
 const bigTarget: TradingPlans.ExitTargets = {
@@ -30,14 +30,32 @@ const bigTarget: TradingPlans.ExitTargets = {
     }
 }
 export const stockSelections: string[] = [
-    'OKTA',
+    'NVDA',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'CVX',
+        symbol: 'TSLA',
         vwapCorrection: { volumeSum: 212225, tradingSum: 34487997 },
-        dailyRange: 3.3,
+        dailyRange: 10,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
+        },
+        short: {
+            openingDrive: { targets: Constants.tslaStockTargets, },
+            momentum: { targets: Constants.tslaStockTargets },
+        },
+        long: {
+            momentum: { targets: Constants.tslaStockTargets },
+            openingDrive: { targets: Constants.tslaStockTargets, },
+        }
+    },
+    {
+        symbol: 'RTX',
+        vwapCorrection: { volumeSum: 206250, tradingSum: 14531543 },
+        dailyRange: 2,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
@@ -48,12 +66,13 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             momentum: { targets: bigTarget },
         },
         long: {
+            openingDrive: { targets: bigTarget, },
             momentum: { targets: bigTarget },
         }
     },
     {
-        symbol: 'OKTA',
-        vwapCorrection: { volumeSum: 206250, tradingSum: 14531543 },
+        symbol: 'COIN',
+        vwapCorrection: { volumeSum: 355795, tradingSum: 29633892 },
         dailyRange: 4.5,
         deferTradingInSeconds: 0,
         keyLevels: {
@@ -63,6 +82,42 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         short: {
             openingDrive: { targets: bigTarget, },
             momentum: { targets: bigTarget },
+        },
+        long: {
+            openingDrive: { targets: bigTarget, },
+            momentum: { targets: bigTarget },
+        }
+    },
+    {
+        symbol: 'COIN',
+        vwapCorrection: { volumeSum: 355795, tradingSum: 29633892 },
+        dailyRange: 4.5,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
+        },
+        short: {
+            openingDrive: { targets: bigTarget, },
+            momentum: { targets: bigTarget },
+        },
+        long: {
+            openingDrive: { targets: bigTarget, },
+            momentum: { targets: bigTarget },
+        }
+    },
+    {
+        symbol: 'NVDA',
+        vwapCorrection: { volumeSum: 256749, tradingSum: 111985870 },
+        dailyRange: 14,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
+        },
+        short: {
+            openingDrive: { targets: smallTarget, },
+            momentum: { targets: smallTarget },
         },
         long: {
             openingDrive: { targets: smallTarget, },
