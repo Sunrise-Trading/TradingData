0b42b2250bd94f44d9db98ff9e4a1c1091e7686c
Rong Ling <wumnlr@gmail.com>
2023-10-27 06:24:07 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -14,46 +14,81 @@ const bigTarget: TradingPlans.ExitTargets = {
     minimumTargets: {
         rrr: [0.9, 0.95, 1, 1.5, 1.8, 1.9, 2, 2, 2, 4],
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        dailyRanges: [0.4, 0.45, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1],
+    }
+}
+const amznTarget: TradingPlans.ExitTargets = {
+    initialTargets: {
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        rrr: [0.9, 0.95, 1, 1.9, 2, 3, 3.5, 4, 5, 10],
+        dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
+    },
+    minimumTargets: {
+        rrr: [0.9, 0.95, 1, 1.5, 1.8, 1.9, 2, 2, 2, 4],
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
     }
 }
 export const stockSelections: string[] = [
-    'META',
+    'AMZN',
+    //'ENPH',
+    //'SNY',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'META',
-        vwapCorrection: { volumeSum: 872937, tradingSum: 251784098 },
-        dailyRange: 11,
+        symbol: 'AMZN',
+        vwapCorrection: { volumeSum: 2602946, tradingSum: 328838743 },
+        dailyRange: 5,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
-            openingDrive: { targets: bigTarget, },
-            momentum: { targets: bigTarget },
+            openingDrive: { targets: amznTarget, },
+            momentum: { targets: amznTarget },
         },
         long: {
-            momentum: { targets: bigTarget },
-            openingDrive: { targets: bigTarget, },
+            momentum: { targets: amznTarget },
+            openingDrive: { targets: amznTarget, },
         }
     },
     {
-        symbol: 'NVDA',
-        vwapCorrection: { volumeSum: 450355, tradingSum: 185133012 },
-        dailyRange: 14,
+        symbol: 'ENPH',
+        vwapCorrection: { volumeSum: 385942, tradingSum: 30548668 },
+        dailyRange: 8.5,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
+            openingDrive: { targets: bigTarget, },
+            momentum: { targets: bigTarget },
+        },
+        long: {
+            openingDrive: { targets: bigTarget, },
+            momentum: { targets: bigTarget },
+        }
+    },
+    {
+        symbol: 'SNY',
+        vwapCorrection: { volumeSum: 2046481, tradingSum: 90898602 },
+        dailyRange: 2.5,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
+        },
+        short: {
+            openingDrive: { targets: bigTarget, },
             momentum: { targets: bigTarget },
         },
         long: {
             momentum: { targets: bigTarget },
+            openingDrive: { targets: bigTarget, },
         }
     },
+
 ];
