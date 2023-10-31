53697ca7d3d6aa98048890fc89376057e90b27d5
Rong Ling <wumnlr@gmail.com>
2023-10-31 06:23:05 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -17,37 +17,56 @@ const bigTarget: TradingPlans.ExitTargets = {
         dailyRanges: [0.4, 0.45, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1],
     }
 }
-const sofiShortTarget: TradingPlans.ExitTargets = {
+const srptTarget: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         rrr: [0.9, 0.95, 1, 1.5, 2, 3, 3.5, 4, 5, 10],
-        dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
+        dailyRanges: [0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.1],
     },
     minimumTargets: {
         rrr: [0.9, 0.95, 1, 1, 1.8, 1.9, 2, 2, 2, 4],
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        dailyRanges: [0.4, 0.4, 0.45, 0.45, 0.5, 0.5, 0.5, 0.6, 0.8, 1],
+        dailyRanges: [0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1],
     }
 }
 export const stockSelections: string[] = [
-    'SOFI',
-    //'TSLA',
+    'SRPT',
+    //'PINS',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'SOFI',
-        autoFlip: true,
-        vwapCorrection: { volumeSum: 4121508, tradingSum: 30470707 },
-        dailyRange: 0.5,
+        symbol: 'SRPT',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 2628498, tradingSum: 160884775 },
+        dailyRange: 8,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
-            openingDrive: { targets: sofiShortTarget, },
-            momentum: { targets: sofiShortTarget },
+            openingDrive: { targets: srptTarget, },
+            momentum: { targets: srptTarget },
+        },
+        long: {
+            momentum: { targets: srptTarget },
+            openingDrive: { targets: srptTarget, },
+        }
+    },
+    {
+        symbol: 'PINS',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 1107317, tradingSum: 32282655 },
+        dailyRange: 1.5,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
+        },
+        short: {
+            openingDrive: { targets: bigTarget, },
+            momentum: { targets: bigTarget },
         },
         long: {
             momentum: { targets: bigTarget },
