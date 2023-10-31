ea116fe81f2a6a0304c633a45fecc42914afb3df
Rong Ling <wumnlr@gmail.com>
2023-10-31 06:24:48 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -20,11 +20,11 @@ const bigTarget: TradingPlans.ExitTargets = {
 const srptTarget: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        rrr: [0.9, 0.95, 1, 1.5, 2, 3, 3.5, 4, 5, 10],
+        rrr: [0.9, 0.95, 1.5, 1.8, 2, 2.3, 2.5, 2.8, 3, 10],
         dailyRanges: [0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.1],
     },
     minimumTargets: {
-        rrr: [0.9, 0.95, 1, 1, 1.8, 1.9, 2, 2, 2, 4],
+        rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 2, 2, 2, 4],
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         dailyRanges: [0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1],
     }
