8ee5f7717e22d282f6cc72a23cf0c7afe3709878
Rong Ling <wumnlr@gmail.com>
2023-11-07 05:55:02 -0800

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -8,11 +8,11 @@ export const tradingSettigns: TradingPlans.TradingSettings = {
 const bigTarget: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        rrr: [0.9, 0.95, 1, 1.9, 2, 3, 3.5, 4, 5, 10],
+        rrr: [0.9, 0.95, 1.5, 1.9, 2, 3, 3.5, 4, 5, 10],
         dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
     },
     minimumTargets: {
-        rrr: [0.9, 0.95, 1, 1.5, 1.8, 1.9, 2, 2, 2, 4],
+        rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 2, 2, 2, 4],
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         dailyRanges: [0.4, 0.45, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1],
     }
@@ -30,83 +30,45 @@ const smallTargets: TradingPlans.ExitTargets = {
     }
 };
 export const stockSelections: string[] = [
-    //'LI',
-    'CLDX',
-    //'TSLA',
-    //'AAPL',
-    // 
-
+    'DDOG',
+    'SNOW',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'AAPL',
+        symbol: 'DDOG',
         autoFlip: false,
         vwapCorrection: { volumeSum: 299373, tradingSum: 52817836 },
-        dailyRange: 3.3,
+        dailyRange: 6.5,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
-            openingDrive: { targets: smallTargets },
-            momentum: { targets: smallTargets },
+            openingDrive: { targets: bigTarget },
+            momentum: { targets: bigTarget },
         },
         long: {
-
+            openingDrive: { targets: bigTarget },
+            momentum: { targets: bigTarget },
         }
     },
     {
-        symbol: 'TSLA',
+        symbol: 'SNOW',
         autoFlip: false,
         vwapCorrection: { volumeSum: 1312024, tradingSum: 294577232 },
-        dailyRange: 9,
+        dailyRange: 10,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
-            momentum: { targets: smallTargets },
-        },
-        long: {
-            momentum: { targets: smallTargets },
-        }
-    },
-    {
-        symbol: 'CLDX',
-        autoFlip: false,
-        vwapCorrection: { volumeSum: 111777, tradingSum: 3938338 },
-        dailyRange: 2.8,
-        deferTradingInSeconds: 0,
-        keyLevels: {
-            momentumStartForLong: 0,
-            momentumStartForShort: 0,
-        },
-        short: {
-            openingDrive: { targets: bigTarget, },
+            openingDrive: { targets: bigTarget },
             momentum: { targets: bigTarget },
         },
         long: {
-        }
-    },
-    {
-        symbol: 'LI',
-        autoFlip: false,
-        vwapCorrection: { volumeSum: 416614, tradingSum: 16400873 },
-        dailyRange: 1.8,
-        deferTradingInSeconds: 0,
-        keyLevels: {
-            momentumStartForLong: 0,
-            momentumStartForShort: 0,
-        },
-        short: {
-            openingDrive: { targets: bigTarget, },
-            momentum: { targets: bigTarget },
-        },
-        long: {
-            momentum: { targets: bigTarget },
         }
     }
 ];
