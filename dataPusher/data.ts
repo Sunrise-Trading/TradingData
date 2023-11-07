148e98a2a02c38f4dc0a1418a428a07595b0458e
Rong Ling <wumnlr@gmail.com>
2023-11-07 07:34:19 -0800

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -20,13 +20,13 @@ const bigTarget: TradingPlans.ExitTargets = {
 const smallTargets: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [],
-        rrr: [0.9, 0.95, 1.6, 1.8, 1.9, 2, 2, 3, 3, 5],
+        rrr: [0.9, 0.95, 1, 1.8, 1.9, 2, 2, 3, 3, 5],
         dailyRanges: [0.8, 0.8, 0.8, 0.8, 0.8, 0.9, 0.9, 1, 1, 1],
     },
     minimumTargets: {
         priceLevels: [],
-        rrr: [0.9, 0.95, 1.5, 1.6, 1.8, 1.8, 1.8, 2.5, 3, 5],
-        dailyRanges: [0.45, 0.45, 0.5, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        rrr: [0.9, 0.95, 1, 1.6, 1.8, 1.8, 1.8, 2.5, 3, 5],
+        dailyRanges: [0.45, 0.45, 0.5, 0.5, 0.5, 0.5, 0.5, 0.8, 0.9, 1],
     }
 };
 export const stockSelections: string[] = [
@@ -46,8 +46,8 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             momentumStartForShort: 0,
         },
         short: {
-            //openingDrive: { targets: bigTarget },
-            momentum: { targets: bigTarget },
+            openingDrive: { targets: smallTargets },
+            momentum: { targets: smallTargets },
         },
         long: {
             openingDrive: { targets: bigTarget },
@@ -65,10 +65,11 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             momentumStartForShort: 0,
         },
         short: {
-            openingDrive: { targets: bigTarget },
-            momentum: { targets: bigTarget },
+            //openingDrive: { targets: bigTarget },
+            //momentum: { targets: bigTarget },
         },
         long: {
+            openingDrive: { targets: bigTarget },
             momentum: { targets: bigTarget },
         }
     }
