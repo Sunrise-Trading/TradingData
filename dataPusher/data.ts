322424317d072d5be282a78417c0f31ed4b3bdbf
Rong Ling <wumnlr@gmail.com>
2023-11-17 05:49:25 -0800

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -23,18 +23,6 @@ const bigTarget: TradingPlans.ExitTargets = {
         dailyRanges: [0.4, 0.45, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1],
     }
 };
-const mTarget: TradingPlans.ExitTargets = {
-    initialTargets: {
-        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        rrr: [0.9, 0.95, 1.5, 1.9, 2, 3, 3.5, 4, 5, 10],
-        dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
-    },
-    minimumTargets: {
-        rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 2, 2, 2, 4],
-        priceLevels: [14.65, 14.65, 14.65, 14.65, 14.65, 14.65, 14.65, 14.65, 14.65, 15],
-        dailyRanges: [0.4, 0.45, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1],
-    }
-}
 const smallTargets: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [],
@@ -48,45 +36,43 @@ const smallTargets: TradingPlans.ExitTargets = {
     }
 };
 export const stockSelections: string[] = [
-    'M',
+    'AMAT',
     'BABA',
-    'WMT',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'M',
+        symbol: 'AMAT',
         autoFlip: false,
         vwapCorrection: { volumeSum: 1703833, tradingSum: 23397606 },
-        dailyRange: 1,
+        dailyRange: 5.5,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
+            momentum: { targets: bigTarget, planConfigs: scalpConfigs },
         },
         long: {
-            openingDrive: { targets: mTarget, planConfigs: bestConfigs },
-            momentum: { targets: mTarget, planConfigs: bestConfigs },
+            momentum: { targets: bigTarget, planConfigs: scalpConfigs },
         },
     },
     {
         symbol: 'BABA',
         autoFlip: false,
         vwapCorrection: { volumeSum: 5022448, tradingSum: 405572246 },
-        dailyRange: 4,
+        dailyRange: 3.5,
         deferTradingInSeconds: 0,
         keyLevels: {
             momentumStartForLong: 0,
             momentumStartForShort: 0,
         },
         short: {
-            openingDrive: { targets: bigTarget, planConfigs: bestConfigs },
-            momentum: { targets: bigTarget, planConfigs: bestConfigs },
+            momentum: { targets: bigTarget, planConfigs: scalpConfigs },
         },
         long: {
-            momentum: { targets: bigTarget, planConfigs: bestConfigs },
+            momentum: { targets: bigTarget, planConfigs: scalpConfigs },
         }
     },
     {
