b5ccc454f9018863007485b0f75d858753041f1f
Rong Ling <wumnlr@gmail.com>
2023-11-21 06:13:04 -0800

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -42,46 +42,80 @@ const smallTargets: TradingPlans.ExitTargets = {
     }
 };
 export const stockSelections: string[] = [
-    'MSFT',
-    'BMY'
+    'TSLA', 'DKS',
+    'AI', 'ZM'
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'MSFT',
+        symbol: 'TSLA',
         autoFlip: false,
-        vwapCorrection: { volumeSum: 1687646, tradingSum: 632490789 },
-        dailyRange: 7,
+        vwapCorrection: { volumeSum: 848648, tradingSum: 200651295 },
+        dailyRange: 9,
         deferTradingInSeconds: 0,
         keyLevels: {
-            momentumStartForLong: 374.37,
-            momentumStartForShort: 366.98,
+            momentumStartForLong: 237.1,
+            momentumStartForShort: 232.7,
         },
         short: {
-            momentum: { targets: bigTarget, planConfigs: bestConfigs },
-            levelBreakout: { entryPrice: 366.98, targets: bigTarget, planConfigs: bestConfigs },
         },
         long: {
-            levelBreakout: { entryPrice: 374.37, targets: bigTarget, planConfigs: scalpConfigs },
-            momentum: { targets: bigTarget, planConfigs: scalpConfigs },
+            momentum: { targets: bigTarget, planConfigs: bestConfigs },
+            levelBreakout: { entryPrice: 237.1, targets: bigTarget, planConfigs: bestConfigs },
         },
     },
     {
-        symbol: 'BMY',
+        symbol: 'AI',
         autoFlip: false,
-        vwapCorrection: { volumeSum: 482751, tradingSum: 23530758 },
-        dailyRange: 2.3,
+        vwapCorrection: { volumeSum: 338936, tradingSum: 9670027 },
+        dailyRange: 2.8,
         deferTradingInSeconds: 0,
         keyLevels: {
-            momentumStartForLong: 49,
-            momentumStartForShort: 48.41,
+            momentumStartForLong: 28.53,
+            momentumStartForShort: 28.31,
         },
         short: {
-            momentum: { targets: bigTarget, planConfigs: bestConfigs },
-            levelBreakout: { entryPrice: 48.11, targets: bigTarget, planConfigs: bestConfigs },
         },
         long: {
             momentum: { targets: bigTarget, planConfigs: scalpConfigs },
+            levelBreakout: { entryPrice: 29, targets: bigTarget, planConfigs: bestConfigs },
+        },
+    },
+    {
+        symbol: 'ZM',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 390237, tradingSum: 25212116 },
+        dailyRange: 2.4,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 65.38,
+            momentumStartForShort: 64.48,
+        },
+        short: {
+            momentum: { targets: bigTarget, planConfigs: bestConfigs },
+            levelBreakout: { entryPrice: 63.6, targets: bigTarget, planConfigs: bestConfigs },
+        },
+        long: {
+
+        },
+    },
+    {
+        symbol: 'DKS',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 152520, tradingSum: 19643577 },
+        dailyRange: 5,
+        deferTradingInSeconds: 0,
+        keyLevels: {
+            momentumStartForLong: 130.44,
+            momentumStartForShort: 128.83,
+        },
+        short: {
+            openingDrive: { targets: bigTarget, planConfigs: bestConfigs },
+            momentum: { targets: bigTarget, planConfigs: bestConfigs },
+            levelBreakout: { entryPrice: 127.47, targets: bigTarget, planConfigs: bestConfigs },
+        },
+        long: {
+
         },
     }
 ];
