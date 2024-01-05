a66f93e176ebd195d99cee9c718a3b4592276226
Rong Ling <wumnlr@gmail.com>
2024-01-04 20:57:23 -0800

    a

--- dataPusher/constants.ts
+++ dataPusher/constants.ts
@@ -114,6 +114,30 @@ export const beyoundAtrTargets: TradingPlans.ExitTargets = {
 }
 
 
+const bigTarget: TradingPlans.ExitTargets = {
+    initialTargets: {
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        rrr: [0.9, 0.95, 1.5, 1.9, 2, 3, 3.5, 4, 5, 10],
+        dailyRanges: [0.5, 0.6, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1, 1.1],
+    },
+    minimumTargets: {
+        rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 2, 2, 2, 4],
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        dailyRanges: [0.4, 0.45, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1],
+    }
+};
+const scaledOutAtrTargets: TradingPlans.ExitTargets = {
+    initialTargets: {
+        priceLevels: [],
+        rrr: [0.9, 0.95, 2, 2, 2, 2, 2, 3, 3, 5],
+        dailyRanges: [0.8, 0.8, 0.8, 0.8, 0.8, 0.9, 0.9, 1, 1, 1],
+    },
+    minimumTargets: {
+        priceLevels: [],
+        rrr: [0.9, 0.95, 1, 1.8, 1.9, 2, 2, 3, 3, 5],
+        dailyRanges: [0.5, 0.5, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0],
+    }
+};
 let futures = [
     {
         symbol: 'MES',
