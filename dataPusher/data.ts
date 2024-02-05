dd3f52d09d8d97e00732a94ace2e34b0f8e23a8e
Rong Ling <wumnlr@gmail.com>
2024-02-05 07:01:12 -0800

    a

---
+++ dataPusher/data.ts
@@ -0,0 +1,153 @@
+import * as TradingPlans from './models';
+import * as Constants from './constants';
+
+export const activeProfileName: string = "momentumSimple";  // futures, momentumSimple;
+export const tradingSettings: TradingPlans.TradingSettings = {
+    equalWeightDivider: 4,
+}
+const el: TradingPlans.PlanConfigs = {
+    size: 0.3,
+    deferTradingInSeconds: 0,
+    requireReversal: true,
+    alwaysAllowStopOutOrFlatten: false,
+    alwaysAllowFirstFewExits: true,
+    allowEarlyExits: false,
+};
+const apd: TradingPlans.PlanConfigs = {
+    size: 0.3,
+    deferTradingInSeconds: 0,
+    requireReversal: true,
+    alwaysAllowStopOutOrFlatten: false,
+    alwaysAllowFirstFewExits: true,
+    allowEarlyExits: false,
+};
+const stock3Configs: TradingPlans.PlanConfigs = {
+    size: 0.15,
+    deferTradingInSeconds: 0,
+    requireReversal: true,
+    alwaysAllowStopOutOrFlatten: false,
+    alwaysAllowFirstFewExits: true,
+    allowEarlyExits: false,
+};
+const stock4Configs: TradingPlans.PlanConfigs = {
+    size: 0.15,
+    deferTradingInSeconds: 0,
+    requireReversal: true,
+    alwaysAllowStopOutOrFlatten: false,
+    alwaysAllowFirstFewExits: true,
+    allowEarlyExits: false,
+};
+
+const R2Target: TradingPlans.ExitTargets = {
+    initialTargets: {
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        rrr: [0.9, 0.95, 1.5, 1.8, 1.85, 1.9, 1.95, 2, 2.5, 3],
+        dailyRanges: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1.1],
+    },
+    minimumTargets: {
+        rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 1.9, 1.9, 1.9, 1.9],
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        dailyRanges: [0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.85, 0.9, 1, 1],
+    }
+};
+const R1Target: TradingPlans.ExitTargets = {
+    initialTargets: {
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        rrr: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        dailyRanges: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    },
+    minimumTargets: {
+        rrr: [0.5, 0.5, 0.5, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9],
+        priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        dailyRanges: [0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.85, 0.9, 1, 1],
+    }
+};
+export const stockSelections: string[] = [
+    'EL',
+    'APD',
+];
+
+export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
+    {
+        symbol: 'EL',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 251549, tradingSum: 38991674 },
+        atr: {
+            average: 4.45,
+            mutiplier: 1.5,
+            minimumMultipler: 0.5,
+        },
+        keyLevels: {
+            momentumStartForLong: 152.53,
+            momentumStartForShort: 161,
+        },
+        short: {
+        },
+        long: {
+            redtoGreenPlan: { strictMode: true, targets: R2Target, planConfigs: el },
+            firstRetracementPlan: { targets: R2Target, planConfigs: el },
+            levelBreakout: { entryPrice: 161, targets: R2Target, planConfigs: el },
+        },
+    },
+    {
+        symbol: 'APD',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 57376, tradingSum: 13475356 },
+        atr: {
+            average: 4.43,
+            mutiplier: 1.5,
+            minimumMultipler: 0.5,
+        },
+        keyLevels: {
+            momentumStartForLong: 225,
+            momentumStartForShort: 240,
+        },
+        short: {
+            redtoGreenPlan: { strictMode: true, targets: R2Target, planConfigs: apd },
+            firstRetracementPlan: { targets: R2Target, planConfigs: apd },
+            falseBreakoutPlan: { price: 252, targets: R2Target, planConfigs: apd },
+        },
+        long: {
+        },
+    },
+    {
+        symbol: 'stock3',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 0, tradingSum: 0 },
+        atr: {
+            average: 2.13,
+            mutiplier: 1.5,
+            minimumMultipler: 0.5,
+        },
+        keyLevels: {
+            momentumStartForLong: 173,
+            momentumStartForShort: 173,
+        },
+        short: {
+            redtoGreenPlan: { strictMode: true, targets: R2Target, planConfigs: stock3Configs },
+        },
+        long: {
+            redtoGreenPlan: { strictMode: true, targets: R2Target, planConfigs: stock3Configs },
+        },
+    },
+    {
+        symbol: 'stock4',
+        autoFlip: false,
+        vwapCorrection: { volumeSum: 0, tradingSum: 0 },
+        atr: {
+            average: 2.13,
+            mutiplier: 1.5,
+            minimumMultipler: 0.5,
+        },
+        keyLevels: {
+            momentumStartForLong: 173,
+            momentumStartForShort: 173,
+        },
+        short: {
+            redtoGreenPlan: { strictMode: true, targets: R2Target, planConfigs: stock4Configs },
+        },
+        long: {
+            redtoGreenPlan: { strictMode: true, targets: R2Target, planConfigs: stock4Configs },
+        },
+    },
+];
