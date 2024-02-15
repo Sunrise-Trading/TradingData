5f3214b3106288833b94309555aa2cf525cfc383
ronglingcode <69086027+ronglingcode@users.noreply.github.com>
2024-02-14 16:15:34 -0800

    a

--- dataPusher/dataTemplate.ts
+++ dataPusher/dataTemplate.ts
@@ -51,7 +51,7 @@ const R2Target: TradingPlans.ExitTargets = {
     minimumTargets: {
         rrr: [0.9, 0.95, 1.3, 1.5, 1.8, 1.9, 1.9, 1.9, 1.9, 1.9],
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        dailyRanges: [0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.85, 0.9, 1, 1],
+        dailyRanges: [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1],
     }
 };
 const R1Target: TradingPlans.ExitTargets = {
