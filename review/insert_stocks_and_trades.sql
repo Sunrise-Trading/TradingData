2d3ab541ce9efc483062ef0b82834d223fdbfd4c
Rong Ling <wumnlr@gmail.com>
2024-06-02 09:22:45 -0700

    a

--- review/insert_stocks_and_trades.sql
+++ review/insert_stocks_and_trades.sql
@@ -10,4 +10,9 @@ insert into [dbo].[daily_trades] (datestr, symbol,daily_stock, playbook, side, m
 values ('2024-05-29','aal','aal-2024-05-29', 'premkt 2nd breakout', 'short',7, 0);
 
 insert into [dbo].[daily_trades] (datestr, symbol,daily_stock, playbook, side, max_r, traded) 
-values ('2024-05-29','crm','crm-2024-05-29', 'green to red > 60', 'short',3,0);
+values ('2024-05-29','chwy','chwy-2024-05-29', 'first new high', 'long',3,0);
+insert into [dbo].[daily_trades] (datestr, symbol,daily_stock, playbook, side, max_r, traded) 
+values ('2024-05-29','chwy','chwy-2024-05-29', 'profit taking exhaust < 60', 'short',-1,1);
+
+insert into [dbo].[daily_trades] (datestr, symbol,daily_stock, playbook, side, max_r, traded) 
+values ('2024-05-30','path','path-2024-05-30', 'first pullback', 'short',1.9,0);
