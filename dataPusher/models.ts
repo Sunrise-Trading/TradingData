db39553d35379d7066025ed7a57c8f545d84e872
Rong Ling <wumnlr@gmail.com>
2024-06-26 10:48:10 -0700

    maxRisk: 0,

--- dataPusher/models.ts
+++ dataPusher/models.ts
@@ -33,6 +33,7 @@ export interface AverageTrueRange {
     average: number,
     mutiplier: number,
     minimumMultipler: number,
+    maxRisk: number
 }
 export interface VwapCorrection {
     volumeSum: number,
