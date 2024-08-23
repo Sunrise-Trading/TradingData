ffe9ca94df4164dea4fed54a93f9dfbdef72de08
Rong Ling <wumnlr@gmail.com>
2024-08-23 07:08:07 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -44,7 +44,7 @@ const stock4Configs: TradingPlans.PlanConfigs = {
     allowFirstFewExitsCount: 2,
 };
 
-const snowTarget: TradingPlans.ExitTargets = {
+const stock1Target: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         rrr: [1.5, 1.6, 1.8, 1.9, 2, 2, 3, 3, 3, 3],
@@ -57,10 +57,10 @@ const snowTarget: TradingPlans.ExitTargets = {
     },
     wave3BatchIndexStart: 10,
     wave5BatchIndexStart: 10,
-    trail5Count: 10,
-    trail15Count: 10,
+    trail5Count: 4,
+    trail15Count: 4,
 };
-const aapTarget: TradingPlans.ExitTargets = {
+const stock2Target: TradingPlans.ExitTargets = {
     initialTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         rrr: [1.5, 1.6, 1.8, 1.9, 2, 2, 3, 3, 3, 3],
@@ -69,12 +69,12 @@ const aapTarget: TradingPlans.ExitTargets = {
     minimumTargets: {
         priceLevels: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         rrr: [1.5, 1.6, 1.8, 1.9, 2, 2, 2, 2, 2, 2],
-        dailyRanges: [1, 1, 1.5, 1.5, 1.9, 1.9, 1.9, 1.9, 1.9, 2],
+        dailyRanges: [1, 1, 1, 1, 1, 1.1, 1.2, 1.3, 1.4, 1.5],
     },
     wave3BatchIndexStart: 10,
     wave5BatchIndexStart: 10,
-    trail5Count: 8,
-    trail15Count: 10,
+    trail5Count: 4,
+    trail15Count: 4,
 };
 const stock3Target: TradingPlans.ExitTargets = {
     initialTargets: {
@@ -89,7 +89,7 @@ const stock3Target: TradingPlans.ExitTargets = {
     },
     wave3BatchIndexStart: 10,
     wave5BatchIndexStart: 10,
-    trail5Count: 8,
+    trail5Count: 4,
     trail15Count: 4,
 };
 const stock4Target: TradingPlans.ExitTargets = {
@@ -109,113 +109,116 @@ const stock4Target: TradingPlans.ExitTargets = {
     trail15Count: 4,
 };
 export const stockSelections: string[] = [
-    'SNOW',
-    'AAP',
+    'CAVA',
+    'WDAY',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'SNOW',
+        symbol: 'CAVA',
         analysis: {
             newsQualityAndFreshness: 2, gapType: TradingPlans.GapType.Outside,
-            relativeVolumeAndCandleSmoothness: 2,
-            cleanVwapTrend: 1, dailyChartStory: 1,
-            gapSize: 15,
-            weeklychart: "consolidation in the bottom range",
-            dailyChart: "popping in a big downtrend",
-            hourlyChart: "rally and pullback into previous support 121-120",
-            premarketChart: "gap down hold and consolidate",
-            keyLevels: [120],
+            relativeVolumeAndCandleSmoothness: 1,
+            cleanVwapTrend: 0, dailyChartStory: 2,
+            gapSize: 8,
+            weeklychart: "uptrend, made new high",
+            dailyChart: "uptrend",
+            hourlyChart: "seems the end of a rally, going round top",
+            premarketChart: "pop and fade",
+            keyLevels: [112.25, 110],
         },
         autoFlip: false,
         vwapCorrection: { volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.SNOW,
+        marketCapInMillions: Constants.marketCaps.CAVA,
         atr: {
-            average: 4.9,
+            average: 4.26,
             mutiplier: 1.5,
             minimumMultipler: 1,
         },
         disableShortIfOpenAbove: 0,
         disableLongIfOpenBelow: 0,
         keyLevels: {
-            momentumStartForLong: 120,
-            momentumStartForShort: 130,
+            momentumStartForLong: 105,
+            momentumStartForShort: 115,
         },
         summary: `
-        earnings beat, raise guidance. not sure why the stock gap down. for long, trade like jd yesterday for a false breakdown
-        initial momentum is bearish, so for long, let it make a short pattern first. ideally like JD yesterday,
-        make a green to red < 60 first. can then don't go lower can get back up.
-        reclaim vwap now, so long only. now, need to wait for the first dip. let the dip reverse and then go long.
+        gap up on earnings beat and raise guidance. charts are bullish. gap up 2 ATR. if open above 110, solid long.
+        if open below 110, still long. gap up is not huge for shorts. but below vwap so far, so keep watching for shorts.
+        candles not smooth, so skip first 60 seconds.
         `,
         short: {
             reasons: [
-                "bearish initial momentum, gap down to break uptrend",
+                "gap up is not that huge. ",
+                ""
             ],
-            /*
-            falseBreakoutPlan: { price: 0, targets: snowTarget, planConfigs: stock1Configs },
-            firstBreakoutPlan: { targets: snowTarget, planConfigs: stock1Configs },
-            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: snowTarget, planConfigs: stock1Configs },
-            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: snowTarget, planConfigs: stock1Configs },
-            firstRetracementPlan: { targets: snowTarget, planConfigs: stock1Configs },*/
+            falseBreakoutPlan: { price: 110, targets: stock1Target, planConfigs: stock1Configs },
+            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: stock1Target, planConfigs: stock1Configs },
+            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: stock1Target, planConfigs: stock1Configs },
+            firstRetracementPlan: { targets: stock1Target, planConfigs: stock1Configs },
         },
         long: {
             reasons: [
-                "earnings beat, raise guidance.",
-                "additional 2.5B buyback",
-                "first dip can get bounght up"
+                " gap up on earnings beat and raise guidance. charts are bullish.",
             ],
-            falseBreakoutPlan: { price: 0, targets: snowTarget, planConfigs: stock1Configs },
-            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: snowTarget, planConfigs: stock1Configs },
-            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: snowTarget, planConfigs: stock1Configs },
-            firstRetracementPlan: { targets: snowTarget, planConfigs: stock1Configs },
+            falseBreakoutPlan: { price: 110, targets: stock1Target, planConfigs: stock1Configs },
+            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: stock1Target, planConfigs: stock1Configs },
+            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: stock1Target, planConfigs: stock1Configs },
+            firstRetracementPlan: { targets: stock1Target, planConfigs: stock1Configs },
         },
     },
     {
-        symbol: 'AAP',
+        symbol: 'WDAY',
         analysis: {
             newsQualityAndFreshness: 2, gapType: TradingPlans.GapType.Outside,
             relativeVolumeAndCandleSmoothness: 1,
-            cleanVwapTrend: 2, dailyChartStory: 2,
-            gapSize: 12,
-            weeklychart: "long term down trend",
-            dailyChart: "downtrend and then consolidate",
-            hourlyChart: "consolidation and big drop",
-            premarketChart: "weak below vwap and extended",
-            keyLevels: [47.73, 50],
+            cleanVwapTrend: 0, dailyChartStory: 2,
+            gapSize: 30,
+            weeklychart: "recover 50% of the pullback",
+            dailyChart: "consolidate and breakout to the next range",
+            hourlyChart: "consolidate and breakout",
+            premarketChart: "kind of pop and fade",
+            keyLevels: [263.66],
         },
         autoFlip: false,
         vwapCorrection: { volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.AAP,
+        marketCapInMillions: Constants.marketCaps.WDAY,
         atr: {
-            average: 2,
-            mutiplier: 2,
+            average: 5.71,
+            mutiplier: 1.5,
             minimumMultipler: 1,
         },
-        disableShortIfOpenAbove: 0,
-        disableLongIfOpenBelow: 0,
+        disableShortIfOpenAbove: 263.66,
+        disableLongIfOpenBelow: 263,
         keyLevels: {
-            momentumStartForLong: 50,
-            momentumStartForShort: 50,
+            momentumStartForLong: 250,
+            momentumStartForShort: 280,
         },
         summary: `
-        gap down huge into all time low after earnings miss. initial momentum is very bearish, so short only.
-        prepare all short setups. If open below all time low, allow green to red < 60,
-        now bouncing, if open near but below 50, also allow green to red < 60 afte a false breakout of 50
+        gap up on earnings beat and raise guidance. daily chart is still kind of bearish. gap up is very big for profit taking.
+        use 263.66 as key level. above it, long, below it, short.
+        trust the open price because it keeps working. if open above 263.66, long only. if open below 263.00, short only
         `,
         short: {
             reasons: [
-                "earnings miss, lower guidance",
+                "daily chart is still kind of bearish. gap up is very big for profit taking.",
+                ""
             ],
-            falseBreakoutPlan: { price: 50, targets: aapTarget, planConfigs: stock2Configs },
-            openDriveContinuation60Plan: { targets: aapTarget, planConfigs: stock2Configs },
-            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: aapTarget, planConfigs: stock2Configs },
-            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: aapTarget, planConfigs: stock2Configs },
-            firstRetracementPlan: { targets: aapTarget, planConfigs: stock2Configs },
+            falseBreakoutPlan: { price: 263.66, targets: stock2Target, planConfigs: stock2Configs },
+            levelBreakout: { entryPrice: 263.66, targets: stock2Target, planConfigs: stock2Configs },
+            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: stock2Target, planConfigs: stock2Configs },
+            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: stock2Target, planConfigs: stock2Configs },
+            firstRetracementPlan: { targets: stock2Target, planConfigs: stock2Configs },
         },
         long: {
             reasons: [
-                "gap down into support",
+                " gap up on earnings beat and raise guidance.",
+                ""
             ],
+            falseBreakoutPlan: { price: 263.66, targets: stock2Target, planConfigs: stock2Configs },
+            levelBreakout: { entryPrice: 263.66, targets: stock2Target, planConfigs: stock2Configs },
+            redtoGreenPlan: { strictMode: true, considerCurrentCandleAfterOneMinute: true, targets: stock2Target, planConfigs: stock2Configs },
+            firstNewHighPlan: { enableAutoTrigger: false, includeSecondNewHigh: true, targets: stock2Target, planConfigs: stock2Configs },
+            firstRetracementPlan: { targets: stock2Target, planConfigs: stock2Configs },
         },
     },
     {
