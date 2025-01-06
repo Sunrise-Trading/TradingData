57761c62827e18c5cf8dccbca4bf987ca27f0804
Rong Ling <wumnlr@gmail.com>
2025-01-05 18:00:38 -0800

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -15,7 +15,8 @@ const stock1Configs: TradingPlans.PlanConfigs = {
     requireReversal: true,
     alwaysAllowFlatten: true,
     alwaysAllowMoveStop: true,
-    allowFirstFewExitsCount: 5,
+    allowFirstFewExitsCount: 3,
+    setupQuality: TradingPlans.SetupQuality.Move2Move,
 };
 const stock2Configs: TradingPlans.PlanConfigs = {
     size: defaultSize,
@@ -24,7 +25,9 @@ const stock2Configs: TradingPlans.PlanConfigs = {
     requireReversal: true,
     alwaysAllowFlatten: true,
     alwaysAllowMoveStop: true,
-    allowFirstFewExitsCount: 5,
+    allowFirstFewExitsCount: 3,
+    setupQuality: TradingPlans.SetupQuality.Move2Move,
+
 };
 const stock3Configs: TradingPlans.PlanConfigs = {
     size: defaultSize,
@@ -33,7 +36,8 @@ const stock3Configs: TradingPlans.PlanConfigs = {
     requireReversal: true,
     alwaysAllowFlatten: true,
     alwaysAllowMoveStop: true,
-    allowFirstFewExitsCount: 5,
+    allowFirstFewExitsCount: 3,
+    setupQuality: TradingPlans.SetupQuality.Move2Move,
 };
 const stock4Configs: TradingPlans.PlanConfigs = {
     size: defaultSize,
@@ -42,7 +46,8 @@ const stock4Configs: TradingPlans.PlanConfigs = {
     requireReversal: true,
     alwaysAllowFlatten: true,
     alwaysAllowMoveStop: true,
-    allowFirstFewExitsCount: 5,
+    allowFirstFewExitsCount: 3,
+    setupQuality: TradingPlans.SetupQuality.Move2Move,
 };
 
 const unlimitTargetForAll: TradingPlans.ExitTargets = {
@@ -142,57 +147,60 @@ const stock4Target: TradingPlans.ExitTargets = {
     trail15Count: 10,
 };
 export const stockSelections: string[] = [
-    'X',
-
-    'RIVN',
-    'RGTI',
-    'CVNA',
+    'stock1',
+    'stock2',
+    'stock3',
+    'stock4',
 ];
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'X',
+        symbol: 'stock1',
         analysis: {
-            newsQualityAndFreshness: 2, gapType: TradingPlans.GapType.Outside,
-            relativeVolumeAndCandleSmoothness: 2,
-            cleanVwapTrend: 1, dailyChartStory: 1,
-            gapSize: 2,
-            weeklychart: "range",
-            dailyChart: "down",
-            hourlyChart: "range, false breakout",
-            premarketChart: "above vwap ",
-            keyLevels: [30.91],
-            singleMomentumKeyLevel: 30.91,
+            isFreshNews: false, newsImpactScore: 0, marketReactionScore: 0, swingHoldRatio: 0,
+            relativeVolumeAndCandleSmoothness: -1,
+            cleanVwapTrend: -1, dailyChartStory: -1,
+            gapSize: 0,
+            weeklychart: "",
+            dailyChart: "",
+            hourlyChart: "",
+            premarketChart: "",
+            keyLevels: [],
+            singleMomentumKeyLevel: -1,
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: { target1: 32.25, target2: 32.4, summary: "half at half, then hold rest into gap fill 32.4" },
-            profitTargetsForShort: { target1: 29.71, target2: 29.38, summary: "first target is daily low 29.71, it's very close, if break lower, go below premarket low" },
-            vwapExtensionDistance: 2,
+            profitTargetsForLong: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            profitTargetsForShort: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            vwapExtensionDistance: 0,
             choppyOpenRangeHigh: 0,
             choppyOpenRangeLow: 0,
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.X,
+        marketCapInMillions: 0,
         atr: {
-            average: 1.89,
-            mutiplier: 1.4,
-            minimumMultipler: 1,
-            maxRisk: 1,
+            average: 0,
+            mutiplier: 0,
+            minimumMultipler: 0,
+            maxRisk: 0,
         },
         disableShortIfOpenAbove: 0,
         disableLongIfOpenBelow: 0,
         keyLevels: {
-            momentumStartForLong: 29.71,
-            momentumStartForShort: 31,
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
         },
         summary: `
-        might be a range trading day between 29.71 to 31.
+        
         `,
         setups: [],
         defaultConfigs: stock1Configs, defaultTargets: stock1Target,
         short: {
             reasons: [
-                "gap down below premarket news rally low",
+                "",
             ],
             levelMomentumPlan: { targets: stock1Target, planConfigs: stock1Configs },
             falseBreakoutPlan: { price: 0, targets: stock1Target, planConfigs: stock1Configs },
@@ -202,7 +210,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
         long: {
             reasons: [
-                "gap down to near daily support 29.71 and hold above vwap",
+                "",
             ],
             levelMomentumPlan: { targets: stock1Target, planConfigs: stock1Configs },
             falseBreakoutPlan: { price: 0, targets: stock1Target, planConfigs: stock1Configs },
@@ -212,48 +220,52 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
     },
     {
-        symbol: 'CVNA',
+        symbol: 'stock2',
         analysis: {
-            newsQualityAndFreshness: 1, gapType: TradingPlans.GapType.Outside,
-            relativeVolumeAndCandleSmoothness: 1,
-            cleanVwapTrend: 1, dailyChartStory: 1,
-            gapSize: 7,
-            weeklychart: "top",
-            dailyChart: "drop",
-            hourlyChart: "downtrend",
-            premarketChart: "gap down bounce above vwap and then lost vwap",
-            keyLevels: [186.86],
-            singleMomentumKeyLevel: 186.86,
+            isFreshNews: false, newsImpactScore: 0, marketReactionScore: 0, swingHoldRatio: 0,
+            relativeVolumeAndCandleSmoothness: -1,
+            cleanVwapTrend: -1, dailyChartStory: -1,
+            gapSize: 0,
+            weeklychart: "",
+            dailyChart: "",
+            hourlyChart: "",
+            premarketChart: "",
+            keyLevels: [],
+            singleMomentumKeyLevel: -1,
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: { target1: 193, target2: 199, summary: "first target is vwap, if hold above vwap, continue to 199-200" },
-            profitTargetsForShort: { target1: 182, target2: 180, summary: "if below 182, potential is $20 move" },
-            vwapExtensionDistance: 10,
+            profitTargetsForLong: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            profitTargetsForShort: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            vwapExtensionDistance: 0,
             choppyOpenRangeHigh: 0,
             choppyOpenRangeLow: 0,
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.CVNA,
+        marketCapInMillions: 0,
         atr: {
-            average: 9.9,
-            mutiplier: 1,
-            minimumMultipler: 1,
-            maxRisk: 3,
+            average: 0,
+            mutiplier: 0,
+            minimumMultipler: 0,
+            maxRisk: 0,
         },
         disableShortIfOpenAbove: 0,
         disableLongIfOpenBelow: 0,
         keyLevels: {
-            momentumStartForLong: 186.86,
-            momentumStartForShort: 186.86,
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
         },
         summary: `
-        because it's a second day news, only trade around reaction low 186.86.
+        
         `,
         setups: [],
         defaultConfigs: stock2Configs, defaultTargets: stock2Target,
         short: {
             reasons: [
-                "gap down forming 2nd leg down",
+                "",
             ],
             levelMomentumPlan: { targets: stock2Target, planConfigs: stock2Configs },
             falseBreakoutPlan: { price: 0, targets: stock2Target, planConfigs: stock2Configs },
@@ -263,7 +275,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
         long: {
             reasons: [
-                "still above reaction low",
+                "",
             ],
             levelMomentumPlan: { targets: stock2Target, planConfigs: stock2Configs },
             falseBreakoutPlan: { price: 0, targets: stock2Target, planConfigs: stock2Configs },
@@ -273,48 +285,52 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
     },
     {
-        symbol: 'RIVN',
+        symbol: 'stock3',
         analysis: {
-            newsQualityAndFreshness: 2, gapType: TradingPlans.GapType.Outside,
-            relativeVolumeAndCandleSmoothness: 2,
-            cleanVwapTrend: 2, dailyChartStory: 1,
-            gapSize: 0.7,
-            weeklychart: "down range",
-            dailyChart: "range",
-            hourlyChart: "range",
-            premarketChart: "above vwap",
-            keyLevels: [13.79],
-            singleMomentumKeyLevel: 13.79,
+            isFreshNews: false, newsImpactScore: 0, marketReactionScore: 0, swingHoldRatio: 0,
+            relativeVolumeAndCandleSmoothness: -1,
+            cleanVwapTrend: -1, dailyChartStory: -1,
+            gapSize: 0,
+            weeklychart: "",
+            dailyChart: "",
+            hourlyChart: "",
+            premarketChart: "",
+            keyLevels: [],
+            singleMomentumKeyLevel: -1,
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: { target1: 14.4, target2: 14.72, summary: "14.4 is a heavy resistance, if breakout, re-entry for 14.72" },
-            profitTargetsForShort: { target1: 13.5, target2: 13.31, summary: "potential give back all the rally from premarket" },
-            vwapExtensionDistance: 1,
+            profitTargetsForLong: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            profitTargetsForShort: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            vwapExtensionDistance: 0,
             choppyOpenRangeHigh: 0,
             choppyOpenRangeLow: 0,
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.RIVN,
+        marketCapInMillions: 0,
         atr: {
-            average: 0.95,
-            mutiplier: 1,
-            minimumMultipler: 1,
-            maxRisk: 0.4,
+            average: 0,
+            mutiplier: 0,
+            minimumMultipler: 0,
+            maxRisk: 0,
         },
         disableShortIfOpenAbove: 0,
         disableLongIfOpenBelow: 0,
         keyLevels: {
-            momentumStartForLong: 13.79,
-            momentumStartForShort: 13.79,
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
         },
         summary: `
-        use y-high as key level for trade. 
+        
         `,
         setups: [],
         defaultConfigs: stock3Configs, defaultTargets: stock3Target,
         short: {
             reasons: [
-                "lost y-high",
+                "",
             ],
             levelMomentumPlan: { targets: stock3Target, planConfigs: stock3Configs },
             falseBreakoutPlan: { price: 0, targets: stock3Target, planConfigs: stock3Configs },
@@ -324,7 +340,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
         long: {
             reasons: [
-                "small gap up above y-high",
+                "",
             ],
             levelMomentumPlan: { targets: stock3Target, planConfigs: stock3Configs },
             falseBreakoutPlan: { price: 0, targets: stock3Target, planConfigs: stock3Configs },
@@ -334,48 +350,52 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
     },
     {
-        symbol: 'RGTI',
+        symbol: 'stock4',
         analysis: {
-            newsQualityAndFreshness: 0, gapType: TradingPlans.GapType.Outside,
-            relativeVolumeAndCandleSmoothness: 1,
-            cleanVwapTrend: 2, dailyChartStory: 1,
-            gapSize: 0.5,
-            weeklychart: "up",
-            dailyChart: "up",
-            hourlyChart: "up",
-            premarketChart: "below vwap",
-            keyLevels: [20, 19.35],
-            singleMomentumKeyLevel: 20,
+            isFreshNews: false, newsImpactScore: 0, marketReactionScore: 0, swingHoldRatio: 0,
+            relativeVolumeAndCandleSmoothness: -1,
+            cleanVwapTrend: -1, dailyChartStory: -1,
+            gapSize: 0,
+            weeklychart: "",
+            dailyChart: "",
+            hourlyChart: "",
+            premarketChart: "",
+            keyLevels: [],
+            singleMomentumKeyLevel: -1,
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: { target1: 20.71, target2: 22, summary: "if breakout premarket high, trailing stop" },
-            profitTargetsForShort: { target1: 19.35, target2: 18.34, summary: "need to break below 19.35 to open more room to the downside" },
-            vwapExtensionDistance: 2,
+            profitTargetsForLong: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            profitTargetsForShort: {
+                targets: [], willBlowPastThoseLevels: -1, summary: `
+                ` },
+            vwapExtensionDistance: 0,
             choppyOpenRangeHigh: 0,
             choppyOpenRangeLow: 0,
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.RGTI,
+        marketCapInMillions: 0,
         atr: {
-            average: 2,
-            mutiplier: 1,
-            minimumMultipler: 1,
-            maxRisk: 1,
+            average: 0,
+            mutiplier: 0,
+            minimumMultipler: 0,
+            maxRisk: 0,
         },
         disableShortIfOpenAbove: 0,
         disableLongIfOpenBelow: 0,
         keyLevels: {
-            momentumStartForLong: 20,
-            momentumStartForShort: 20,
+            momentumStartForLong: 0,
+            momentumStartForShort: 0,
         },
         summary: `
-        use 20 as key level.
+        
         `,
         setups: [],
         defaultConfigs: stock4Configs, defaultTargets: stock4Target,
         short: {
             reasons: [
-                "profit taking",
+                "",
             ],
             levelMomentumPlan: { targets: stock4Target, planConfigs: stock4Configs },
             falseBreakoutPlan: { price: 0, targets: stock4Target, planConfigs: stock4Configs },
@@ -385,7 +405,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
         long: {
             reasons: [
-                "all time high breakout",
+                "",
             ],
             levelMomentumPlan: { targets: stock4Target, planConfigs: stock4Configs },
             falseBreakoutPlan: { price: 0, targets: stock4Target, planConfigs: stock4Configs },
