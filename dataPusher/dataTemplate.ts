fe1ef692951332dad76ecf68d7cbc529de7de9b1
Niko <63621856+niko-2020@users.noreply.github.com>
2025-02-18 07:56:07 -0800

    update

--- dataPusher/dataTemplate.ts
+++ dataPusher/dataTemplate.ts
@@ -7,7 +7,7 @@ export const tradingSettings: TradingPlans.TradingSettings = {
     snapMode: false,
     useSingleOrderForEntry: true,
 }
-export const defaultSize = 0.24;
+export const defaultSize = 0.23;
 const stock1Configs: TradingPlans.PlanConfigs = {
     size: defaultSize,
     deferTradingInSeconds: 0,
