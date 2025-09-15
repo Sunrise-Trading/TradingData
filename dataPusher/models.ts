7ec9857010ce0a7c4f039b86b02be0001667b373
Rong Ling <wumnlr@gmail.com>
2025-09-15 16:58:58 -0700

    a

--- dataPusher/models.ts
+++ dataPusher/models.ts
@@ -91,6 +91,7 @@ export interface SingleDirectionPlans {
 export interface VwapBounceFailPlan extends BasePlan { }
 export interface VwapScalpPlan extends BasePlan {
     threshold: number,
+    strongReasonToUseThisLevel: string,
 }
 export interface keyLevels {
     otherLevels?: number[];
