d5dc204805b5fdd82179502ed17462fce23e47f0
Rong Ling <wumnlr@gmail.com>
2025-12-28 16:53:09 -0800

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -149,7 +149,10 @@ export const getGoogleDocId = () => {
     return docId;
 }
 export const stockSelections: string[] = [
-    'NVDA',
+    'stock1',
+    'stock2',
+    'stock3',
+    'stock4',
 ];
 const stock1Level = 1;
 const stock2Level = 1;
@@ -158,7 +161,7 @@ const stock4Level = 1;
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'NVDA',
+        symbol: 'stock1',
         analysis: {
             dailyChartStory: -1,
             gap: { pdc: 0 },
@@ -169,13 +172,6 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             singleMomentumKeyLevel: [{ high: stock1Level, low: stock1Level }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-            profitTargetsForShort: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
@@ -253,13 +249,6 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             singleMomentumKeyLevel: [{ high: stock2Level, low: stock2Level }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-            profitTargetsForShort: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
@@ -337,13 +326,6 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             singleMomentumKeyLevel: [{ high: stock3Level, low: stock3Level }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-            profitTargetsForShort: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
@@ -421,13 +403,6 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             singleMomentumKeyLevel: [{ high: stock4Level, low: stock4Level }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
-            profitTargetsForLong: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-            profitTargetsForShort: {
-                targets: [], willBlowPastThoseLevels: -1, summary: `
-                ` },
-
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
