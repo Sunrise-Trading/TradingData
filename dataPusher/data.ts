66354a7d9d677bbe1b37a3760bc0cc2c45c8a4ce
Rong Ling <lingrong1988@gmail.com>
2026-03-13 08:00:20 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -9,8 +9,8 @@ export const tradingSettings: TradingPlans.TradingSettings = {
 }
 
 export const defaultSize = 0.21; // 0.21
-const stock1Configs: TradingPlans.PlanConfigs = {
-    size: defaultSize,
+const adbeConfigs: TradingPlans.PlanConfigs = {
+    size: 0.05,
     deferTradingInSeconds: 0,
     stopTradingAfterSeconds: 0,
     requireReversal: true,
@@ -149,93 +149,102 @@ export const getGoogleDocId = () => {
     return docId;
 }
 export const stockSelections: string[] = [
-    'stock1',
-    'stock2',
-    'stock3',
-    'stock4',
+    'ADBE',
 ];
-const stock1Level = 1;
+const adbepmhigh = 251;
+const recentlow = 244.28;
+const ADBElevel = 248.31;
+const risklevel: string[] = ["255", "pm high", "pm low"];
 const stock2Level = 1;
 const stock3Level = 1;
 const stock4Level = 1;
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'stock1',
+        symbol: 'ADBE',
         analysis: {
-            dailyChartStory: -1,
-            gap: { pdc: 0 },
+            dailyChartStory: 1,
+            gap: { pdc: 270 },
             dailySetup: TradingPlans.DailySetup.TwoWayOpen,
-            deferTradingInSeconds: -1,
-            stopTradingAfterSeconds: -1,
+            deferTradingInSeconds: 0,
+            stopTradingAfterSeconds: 0,
             usePremarketKeyLevel: 0,
             watchAreas: [],
             noTradeZones: [],
-            singleMomentumKeyLevel: [{ high: stock1Level, low: stock1Level }],
+            singleMomentumKeyLevel: [{ high: ADBElevel, low: ADBElevel }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
         },
         autoFlip: false,
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: 0,
+        marketCapInMillions: Constants.marketCaps.ADBE,
         atr: {
-            average: 0,
-            mutiplier: 0,
-            minimumMultipler: 0,
-            maxRisk: 0,
+            average: 9.6,
+            mutiplier: 1.5,
+            minimumMultipler: 1,
+            maxRisk: 10,
             maxQuantity: -1,
         },
         keyLevels: {
-
+            otherLevels: [ADBElevel, recentlow]
         },
-        defaultConfigs: stock1Configs, defaultTargets: stock1Target,
+        defaultConfigs: adbeConfigs, defaultTargets: stock1Target,
         tradebooksConfig: {
             level_vwap_open: {
-                shortVwapContinuation: {},
-                longEmergingStrengthBreakout: { waitForClose: true, allowCloseWithin: false, },
+                shortVwapContinuation: { enabled: 1, },
+                longEmergingStrengthBreakout: { enabled: 1, waitForClose: true, allowCloseWithin: false, },
             },
             level_open_vwap: {
-                shortVwapBounceFail: { waitForClose: true, },
-                longAboveWaterBreakout: { waitForClose: true, allowCloseWithin: false, },
+                shortVwapBounceFail: { enabled: 1, waitForClose: true, },
+                longAboveWaterBreakout: { enabled: 1, waitForClose: true, allowCloseWithin: false, },
                 shortOpenFlush: {},
                 longVwapScalp: {},
             },
             open_level_vwap: {
-                shortVwapBounceFail: { waitForClose: true, },
-                longOpenDrive: {},
+                shortVwapBounceFail: { enabled: 1, waitForClose: true, },
+                longOpenDrive: { enabled: 1, },
             },
             vwap_level_open: {
-                shortOpenDrive: {},
-                longVwapPushdownFail: { waitForClose: true },
+                shortOpenDrive: { enabled: 1, },
+                longVwapPushdownFail: { enabled: 1, waitForClose: true },
             },
             vwap_open_level: {
-                shortBelowWaterBreakout: { waitForClose: true, allowCloseWithin: false },
-                longVwapPushdownFail: { waitForClose: true },
+                shortBelowWaterBreakout: { enabled: 1, waitForClose: true, allowCloseWithin: false },
+                longVwapPushdownFail: { enabled: 1, waitForClose: true },
             },
             open_vwap_level: {
-                longVwapContinuation: {},
-                shortEmergingWeaknessBreakdown: { waitForClose: true, allowCloseWithin: false },
+                longVwapContinuation: { enabled: 1, },
+                shortEmergingWeaknessBreakdown: { enabled: 1, waitForClose: true, allowCloseWithin: false },
             },
         },
         short: {
             enabled: true,
-            firstTargetToAdd: 0,
+            firstTargetToAdd: -1,
             finalTargets: [
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
+                { text: `${recentlow}`, partialCount: 2, atr: 0, rrr: 0, level: recentlow },
+                { text: "235", partialCount: 2, atr: 0, rrr: 0, level: 235 },
             ],
-            levelMomentumPlan: { enableAutoTrigger: false, targets: stock1Target, planConfigs: stock1Configs },
-            firstNewHighPlan: { enableAutoTrigger: false, targets: stock1Target, planConfigs: stock1Configs },
+            levelMomentumPlan: { defaultRiskLevels: risklevel, enableAutoTrigger: false, targets: stock1Target, planConfigs: adbeConfigs },
+            firstNewHighPlan: { enableAutoTrigger: false, targets: stock1Target, planConfigs: adbeConfigs },
+            gapDownAndGoDownPlan: {
+                buyersTrappedBelowThisLevel: 267,
+                defaultRiskLevels: risklevel, targets: stock1Target, planConfigs: adbeConfigs
+            },
         },
         long: {
             enabled: true,
-            firstTargetToAdd: 0,
+            firstTargetToAdd: -1,
             finalTargets: [
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
+                { text: `${adbepmhigh}`, partialCount: 2, atr: 0, rrr: 0, level: adbepmhigh },
+                { text: "255", partialCount: 2, atr: 0, rrr: 0, level: 255 },
             ],
-            levelMomentumPlan: { enableAutoTrigger: false, targets: stock1Target, planConfigs: stock1Configs },
-            firstNewHighPlan: { enableAutoTrigger: false, targets: stock1Target, planConfigs: stock1Configs },
+            levelMomentumPlan: { defaultRiskLevels: risklevel, enableAutoTrigger: false, targets: stock1Target, planConfigs: adbeConfigs },
+            firstNewHighPlan: { enableAutoTrigger: false, targets: stock1Target, planConfigs: adbeConfigs },
+            gapDownAndGoUpPlan: {
+                nearAboveSupport: { high: 245, low: 244.28 },
+                support: [{ high: 245, low: 244.28 }],
+                defaultRiskLevels: risklevel, targets: stock1Target, planConfigs: adbeConfigs
+            },
         }
     },
     {
