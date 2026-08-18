3b319c7755947a2b298915a3399e93de697fe84c
Rong Ling <wumnlr@gmail.com>
2026-08-18 06:22:34 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -45,41 +45,39 @@ const stock4Configs: TradingPlans.PlanConfigs = {
 
 
 export const stockSelections: string[] = [
-    'NU',
+    'MU',
 ];
 
-const nupreergap = 15.94;
+const lastsupport = 930;
 const moreberish = 15.81;
-const irenpmhigh = 46.79;
-const nulevel = nupreergap;
-const onontrappedlevel = 34.67;
-const ononpreerlow = 31.62;
-const ononlevel = ononpreerlow;
+const mulevel = lastsupport;
+const mupmlow = 956;
+const mupmhigh = 995;
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'NU',
+        symbol: 'AMU',
         analysis: {
-            gap: { pdc: 13.93 },
+            gap: { pdc: 1011 },
             usePremarketKeyLevel: 0,
             watchAreas: [],
             noTradeZones: [],
-            singleMomentumKeyLevel: [{ high: nulevel, low: nulevel }],
+            singleMomentumKeyLevel: [{ high: mulevel, low: mulevel }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
             defaultRiskLevels: [],
         },
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.NU,
+        marketCapInMillions: Constants.marketCaps.MU,
         atr: {
-            average: 0.47,
-            mutiplier: 2,
-            minimumMultipler: 1,
+            average: 70,
+            mutiplier: 1,
+            minimumMultipler: 0.5,
             maxQuantity: -1,
         },
         keyLevels: {
             zones: [
-                { high: 16.85, low: 16.5, color: "red", label: "main profit target" }
+                //{ high: 16.85, low: 16.5, color: "red", label: "main profit target" }
             ]
         },
         defaultConfigs: stock1Configs,
@@ -108,7 +106,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             ],
             gapAndCrapPlan: {
                 extendedGapUpInAtr: 4,
-                resistance: { high: nupreergap, low: moreberish },
+                resistance: { high: lastsupport, low: moreberish },
                 runnerCount: 1,
                 coreCount: 1,
                 runnerTriggerCondition: "lost premarket low",
@@ -128,7 +126,7 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 { text: "16.8", partialCount: 1, atr: 1, rrr: 0, level: 16.8 },
             ],
             gapAndGoPlan: {
-                support: { high: 16, low: nupreergap },
+                support: { high: 16, low: lastsupport },
                 enableOfferBreakout: false,
                 enableBidReversal: true,
                 waitForPullback: true,
@@ -143,28 +141,30 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
         },
     },
     {
-        symbol: 'ONON',
+        symbol: 'MU',
         analysis: {
-            gap: { pdc: 38.78 },
+            gap: { pdc: 1011 },
             usePremarketKeyLevel: 0,
             watchAreas: [],
             noTradeZones: [],
-            singleMomentumKeyLevel: [{ high: ononlevel, low: ononlevel }],
+            singleMomentumKeyLevel: [{ high: mulevel, low: mulevel }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
             defaultRiskLevels: [],
         },
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.GME,
+        marketCapInMillions: Constants.marketCaps.MU,
         atr: {
-            average: 1.39,
-            mutiplier: 1.5,
-            minimumMultipler: 1,
+            average: 72,
+            mutiplier: 1,
+            minimumMultipler: 0.5,
             maxQuantity: -1,
         },
         keyLevels: {
             otherLevels: [],
-            zones: [{ high: ononpreerlow, low: 31, color: "red", label: "short momo" }]
+            zones: [
+                { high: mupmlow + 5, low: mupmlow, color: "green", label: "pm low" }
+            ]
         },
         defaultConfigs: stock2Configs,
         tradebooksConfig: {
@@ -184,12 +184,13 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
             },
         },
         short: {
-            enabled: true,
+            enabled: false,
             firstTargetToAdd: -1,
             finalTargets: [
                 { text: "30", partialCount: 1, atr: 0, rrr: 0, level: 30 },
                 { text: "29", partialCount: 1, atr: 0, rrr: 0, level: 29 },
             ],
+            /*
             gapDownAndGoDownPlan: {
                 buyersTrappedBelowThisLevel: onontrappedlevel,
                 resistance: { high: onontrappedlevel, low: 34 },
@@ -201,15 +202,15 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 coreCount: 1,
                 coreTarget: 30,
                 planConfigs: stock2Configs,
-            },
+            },*/
             levelMomentumPlan: createDefaultLevelMomentumPlan(stock2Configs),
         },
         long: {
             enabled: true,
             firstTargetToAdd: -1,
             finalTargets: [
-                { text: "33.5", partialCount: 1, atr: 0, rrr: 0, level: 33.5 },
-                { text: "34", partialCount: 1, atr: 0, rrr: 0, level: 34 },
+                { text: "pm high", partialCount: 1, atr: 0, rrr: 0, level: mupmhigh },
+                { text: "R4", partialCount: 1, atr: 0, rrr: 0, level: 978 },
             ],
             gapDownAndGoUpPlan: {
                 planConfigs: stock2Configs,
@@ -218,10 +219,10 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 enableOfferBreakout: false,
                 runnerCount: 1,
                 coreCount: 1,
-                support: { high: 32, low: ononpreerlow },
-                nearAboveSupport: { high: 32, low: ononpreerlow },
+                support: { high: 940, low: mulevel },
+                nearAboveSupport: { high: 940, low: mulevel },
                 runnerTriggerCondition: "reclaim vwap",
-                coreTarget: 33.5,
+                coreTarget: mupmhigh,
             },
             levelMomentumPlan: createDefaultLevelMomentumPlan(stock2Configs),
         },
