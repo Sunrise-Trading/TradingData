93cf634a04d66fa3aa5b1a3efb2350ec4b9d867b
Rong Ling <lingrong1988@gmail.com>
2026-08-27 06:12:11 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -34,31 +34,30 @@ const stock2Configs: TradingPlans.PlanConfigs = {
 };
 
 export const stockSelections: string[] = [
-    'stock1',
-    'stock2',
+    'CRM',
 ];
-const stock1Level = 1;
+const crmgaplevel = 227.67;
 const stock2Level = 1;
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'stock1',
+        symbol: 'CRM',
         analysis: {
-            gap: { pdc: 0 },
+            gap: { pdc: 205 },
             usePremarketKeyLevel: 0,
             watchAreas: [],
             noTradeZones: [],
-            singleMomentumKeyLevel: [{ high: stock1Level, low: stock1Level }],
+            singleMomentumKeyLevel: [{ high: crmgaplevel, low: crmgaplevel }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
             defaultRiskLevels: [],
         },
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: 0,
+        marketCapInMillions: Constants.marketCaps.CRM,
         atr: {
-            average: 0,
-            mutiplier: 0,
-            minimumMultipler: 0,
+            average: 7.7,
+            mutiplier: 1.5,
+            minimumMultipler: 1,
             maxQuantity: -1,
         },
         keyLevels: { zones: [] },
@@ -79,23 +78,43 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 longVwapPushdownFail: { waitForClose: true },
             },
         },
-        corePlan: "",
+        corePlan: "CRM gapped up to a previous gap level. I will long above 226.67, target is 235-236. I will also short below 226.67, target is 217-218.",
         short: {
             enabled: true,
-            firstTargetToAdd: "0",
+            firstTargetToAdd: "219",
             finalTargets: [
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
+                { text: "219", partialCount: 1, atr: 0, rrr: 0, level: 219 },
+                { text: "218", partialCount: 1, atr: 0, rrr: 0, level: 218 },
             ],
+            gapAndCrapPlan: {
+                resistance: { high: crmgaplevel, low: 226 },
+                extendedGapUpInAtr: 2.7,
+                coreCount: 1,
+                coreTarget: 218,
+                runnerCount: 1,
+                runnerTriggerCondition: "breakdown/reject below vwap",
+                waitForPullback: false,// if open price is below
+                planConfigs: stock1Configs,
+            },
             levelMomentumPlan: createDefaultLevelMomentumPlan(stock1Configs),
         },
         long: {
             enabled: true,
-            firstTargetToAdd: "0",
+            firstTargetToAdd: "-1",
             finalTargets: [
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
-                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
+                { text: "230", partialCount: 1, atr: 0, rrr: 0, level: 230 },
+                { text: "235", partialCount: 1, atr: 0, rrr: 0, level: 235 },
             ],
+            gapAndGoPlan: {
+                support: { high: 230, low: crmgaplevel },
+                nearPreviousKeyEventLevel: `${crmgaplevel}`,
+                coreCount: 1,
+                coreTarget: 235,
+                runnerCount: 1,
+                runnerTriggerCondition: "pullback hold above premarket high",
+                planConfigs: stock1Configs,
+                waitForPullback: true,
+            },
             levelMomentumPlan: createDefaultLevelMomentumPlan(stock1Configs),
         },
     },
