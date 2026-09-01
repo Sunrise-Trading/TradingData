4492c5d943c7baefee351f19c2575fd940d947f5
Rong Ling <lingrong1988@gmail.com>
2026-09-01 07:46:15 -0700

    a

--- dataPusher/data.ts
+++ dataPusher/data.ts
@@ -34,31 +34,31 @@ const stock2Configs: TradingPlans.PlanConfigs = {
 };
 
 export const stockSelections: string[] = [
-    'PCG',
+    'stock1',
+    'stock2',
 ];
-const pcgerlow = 13.32;
-const pcglevel = 15.28;
+const stock1Level = 1;
 const stock2Level = 1;
 
 export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
     {
-        symbol: 'PCG',
+        symbol: 'stock1',
         analysis: {
-            gap: { pdc: 16.6 },
+            gap: { pdc: 0 },
             usePremarketKeyLevel: 0,
             watchAreas: [],
             noTradeZones: [],
-            singleMomentumKeyLevel: [{ high: pcglevel, low: pcglevel }],
+            singleMomentumKeyLevel: [{ high: stock1Level, low: stock1Level }],
             zoneNearEdge: { zoneIsFar: true, high: 0, low: 0 },
             dualMomentumKeyLevels: [],
             defaultRiskLevels: [],
         },
         vwapCorrection: { open: 0, volumeSum: 0, tradingSum: 0 },
-        marketCapInMillions: Constants.marketCaps.PCG,
+        marketCapInMillions: 0,
         atr: {
-            average: 0.66,
-            mutiplier: 2,
-            minimumMultipler: 1,
+            average: 0,
+            mutiplier: 0,
+            minimumMultipler: 0,
             maxQuantity: -1,
         },
         keyLevels: { zones: [] },
@@ -79,41 +79,23 @@ export const stocksTradingPlans: TradingPlans.TradingPlans[] = [
                 longVwapPushdownFail: { waitForClose: true },
             },
         },
-        corePlan: "Long: open above 13.32, dip quickly to 13.32 and reversal. target 14-14.3. Short: If open below 13.32, short breakdown. If open above 13.32, either wait for a pop to near under 14 or confirmed breakdown below 13.32. target 13, then 12",
+        corePlan: "",
         short: {
             enabled: true,
-            firstTargetToAdd: "13",
+            firstTargetToAdd: "0",
             finalTargets: [
-                { text: "13", partialCount: 1, atr: 0, rrr: 0, level: 13 },
-                { text: "12", partialCount: 1, atr: 0, rrr: 0, level: 12 },
+                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
+                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
             ],
-            gapDownAndGoDownPlan: {
-                planConfigs: stock1Configs,
-                buyersTrappedBelowThisLevel: pcglevel,
-                resistance: { high: pcglevel, low: 15, },
-                coreCount: 3,
-                coreTarget: 13,
-                runnerCount: 3,
-                runnerTriggerCondition: "stay below pm low",
-            },
             levelMomentumPlan: createDefaultLevelMomentumPlan(stock1Configs),
         },
         long: {
             enabled: true,
-            firstTargetToAdd: "14",
+            firstTargetToAdd: "0",
             finalTargets: [
-                { text: "14", partialCount: 1, atr: 0, rrr: 0, level: 14 },
-                { text: "14.25", partialCount: 1, atr: 0, rrr: 0, level: 14.25 },
+                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
+                { text: "", partialCount: 5, atr: 0, rrr: 0, level: 0 },
             ],
-            gapDownAndGoUpPlan: {
-                planConfigs: stock1Configs,
-                support: { low: pcgerlow, high: 13.5 },
-                coreCount: 1,
-                coreTarget: 14,
-                runnerCount: 1,
-                runnerTriggerCondition: "hold above vwap",
-                nearAboveKeyEventLevel: pcgerlow,
-            },
             levelMomentumPlan: createDefaultLevelMomentumPlan(stock1Configs),
         },
     },
